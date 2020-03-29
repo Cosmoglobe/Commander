45eeeb1c0c1eac79f471ff17d85a695fd033278d
Duncan <duncanjosephwatts@gmail.com>
2020-03-29 22:50:37 +0200

    Wrote IDL script to get gain as a function of time from three mnemonics. Not sure if it makes sense...

--- src/todscripts/wmap/get_gain_model.py
+++ src/todscripts/wmap/get_gain_model.py
@@ -20,6 +20,69 @@ fits table ("index" and "arr").
 AIHK_GetMnemonic.pro returns the physical value associated with a mnemonic.
 '''
 
+mnem_list = np.loadtxt('mnem_indices.txt', delimiter=',', dtype=str)
+
+def aihk_mnemonic(mnem):
+    for i in range(len(mnem_list)):
+        if mnem in mnem_list[i,0]:
+            ind = i
+    index, arr = mnem_list[ind,1:]
+    # index - 1 because the indexig is from Fortran
+    return int(index)-1, int(arr)
+
+
+def get_resist(data, index, arr):
+    Win = 0
+    Wflg = 0
+    if arr == 3:
+        Tmp = data[3].data['AEU2'][:, index]
+        if (index >= 1) and (index <= 31):
+            Wflg = 1
+            Win = data[3].data['AWIN2'][:,index//2]
+            # This is some kind of hexadecimal code, but it seems like it's a
+            # correction on the order of 30 K. For the sake of my sanity, I'm going
+            # to set terms involving Win to zero.
+            if index % 2 == 0:
+                Win = np.array([w & 0x00FF for w in Win])
+            else:
+                Win = np.array([(w & 0xFF00)*2**-8 for w in Win])
+            Slp  = 254.968244e-6 
+            YInt = 319.5004
+            WInt = 129
+            WSlp = 256.0
+            Rmax = 650.25838
+            Rmax = 0
+    elif arr == 2:
+        Tmp = data[3].data['AEU1'][:, index]
+        # For AEU1:
+        if (index >= 1) and (index <= 31):
+            Wflg = 2
+            Win = data[3].data['AWIN1'][:,index//2]
+            if index % 2 == 0:
+                Win = np.array([w & 0x00FF for w in Win])
+            else:
+                Win = np.array([(w & 0xFF00)*2**-8 for w in Win])
+            Slp  = 255.381467e-6
+            YInt = 319.5197
+            WInt = 129
+            WSlp = 256.
+            Rmax = 650.58226
+    else:
+        Tmp = data[3].data['PDU'][index]
+    if Wflg == 0:
+        return Tmp
+    else:
+        Res = Tmp*Slp + YInt + Rmax*(Win - WInt) / WSlp
+
+        # from aihk_mnem2serial_list.pro
+        SerialNum = 'UG89'
+        SensorId  = 'IHK1_AC_17'
+        # from get_prt_temp.pro, converts resistance in ohms to temperature in Kelvin.
+        # This requires using the prt_splinedata.xdr file in the ref directory.A
+        
+        Temp = get_prt_temp(Res, SerialNum)
+        return Temp
+
 def get_prt_temp(resist, SerialNum):
     table = readsav('wmap_routines/ref/prt_data/prt_splinedata.xdr')
     splinedata = table['splinedata']
@@ -55,85 +118,103 @@ def G(V, T_RXB, T_FPA, t, pars):
     return G
 
 
-# there are several steps to get from the raw TOD to physical units.
+def get_val_from_mnem(data, mnem):
+    # there are several steps to get from the raw TOD to physical units.
+    
+    # First read in the data themselves, which are indexed according to the
+    # mnemonics mentioned before.
+    
+    
+    # Second, convert the raw value into physical units using the polynomial
+    # conversion in line 122 of aihk_get_mnemonic.pro.
+    
+    # Finally, you need to get the serial number of the thermistor used to measure
+    # the temperature, and get that thermistor's conversion to temperature.
+    
+    # I'll do this manually as an example, for K1 A side feed.
+    
+    # Thinking of doing something like K1A and then.
+    #mnem = get_mnem(band, variable)
+    
+    # index = 18 in fortran indexing, so 17 in idl. Also, AEU1.
+    
+    index, arr = aihk_mnemonic(mnem)
+    # index, arr = 17, 2
+    
+    
+    # Get temperature from index
+    Temp = get_resist(data, index, arr)
+    
+    coefs_array = np.loadtxt('mnem_coefs.txt', dtype=str, delimiter=',')
+    for i in range(len(coefs_array)):
+        if mnem in coefs_array[i,0]:
+            ind = i
+    coefs = coefs_array[ind,1:].astype('float')
+    T_pow = np.array([Temp**i for i in range(len(coefs))])
+    Val = coefs.dot(T_pow)
 
-# First read in the data themselves, which are indexed according to the
-# mnemonics mentioned before.
-
-
-# Second, convert the raw value into physical units using the polynomial
-# conversion in line 122 of aihk_get_mnemonic.pro.
-
-# Finally, you need to get the serial number of the thermistor used to measure
-# the temperature, and get that thermistor's conversion to temperature.
-
-# I'll do this manually as an example, for K1 A side feed.
-
-mnem = 'DFK1AFEEDT'
-
-# index = 18 in fortran indexing, so 17 in idl. Also, AEU1.
-
-# index, arr = aihk_mnemonic(mnem)
-index, arr = 17, 2
-
-data = fits.open('wmap.fits')
-
-# Get raw telemtry value
-Win = 0
-Wflg = 0
-if arr == 3:
-    Tmp = data[3].data['AEU2'][:, index]
-    if (index >= 1) and (index <= 31):
-        Wlfg = 1
-        Win = data[3].data['AWIN2'][:,index//2]
-        # This is some kind of hexadecimal code, but it seems like it's a
-        # correction on the order of 30 K. For the sake of my sanity, I'm going
-        # to set terms involving Win to zero.
-        '''
-        if (index % 2) != 1:
-            Win = (Win & '00FF'xL)
-        else:
-            Win = ISHFT((Win & 'FF00'xL), -8)
-        '''
-        Slp  = 254.968244e-6 
-        YInt = 319.5004
-        WInt = 129
-        WSlp = 256.0
-        Rmax = 650.25838
-        Rmax = 0
-elif arr == 2:
-    Tmp = data[3].data['AEU1'][:, index]
-    # For AEU1:
-    Win = data[3].data['AWIN1'][:,index//2]
-    print(Win)
-    Slp  = 255.381467e-06
-    YInt = 319.5197
-    WInt = 129
-    WSlp = 256.
-    Rmax = 650.58226
-    Rmax = 0
-else:
-    Tmp = data[3].data['PDU'][index]
-  
+    return Val
 
 
-Res = Tmp*Slp + YInt + Rmax*(Win - WInt) / WSlp
+if __name__ == '__main__':
+    data = fits.open('wmap.fits')
+
+    mnem = 'DFK1AFEEDT'
+    T_FPA = get_val_from_mnem(data, mnem)
+
+    plt.figure()
+    plt.subplot(221)
+    plt.plot(data[3].data['TIME'], T_FPA, '.')
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$T$ (Kelvin)')
+
+    T_RXB = get_val_from_mnem(data, 'DRK12RXBRIBT')
+    plt.subplot(222)
+    plt.plot(data[3].data['TIME'], T_RXB, '.')
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$T$ (Kelvin)')
 
 
+    RF_bias = get_val_from_mnem(data, 'DRK113RFBI0')
+    plt.subplot(223)
+    plt.plot(data[3].data['TIME'], RF_bias, '.')
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'RF bias')
 
 
-# from aihk_mnem2serial_list.pro
-SerialNum = 'UG89'
-SensorId  = 'IHK1_AC_17'
+    # Let's see if the gain model makse sense for K13
+    T0 = 5.3235e1
+    V0 = -5.5125e-1
+    beta = -7.22e-4
+    alpha = 4.4619e1
+    m = -2.7274e-7
+    c = -6.7556e-1
+    pars = np.array([T0, V0, beta, alpha, m ,c])
 
-# from get_prt_temp.pro, converts resistance in ohms to temperature in Kelvin.
-# This requires using the prt_splinedata.xdr file in the ref directory.A
+    G_K1 = G(RF_bias, T_RXB, T_FPA, data[3].data['TIME'], pars)
+    plt.subplot(224)
+    plt.plot(data[3].data['TIME'], G_K1)
 
-Temp = get_prt_temp(Res, SerialNum)
 
-plt.figure()
-plt.plot(data[3].data['TIME'], Temp)
-plt.xlabel(r'$t$')
-plt.ylabel(r'$T$ (Kelvin)')
+    # The only published gain model I've seen in the WMAP paper is Figure 2 of
+    # Jarosik et al. 2007, which plots the V223 detector.
 
-plt.show()
+    mnem_rfb = 'DRV223RFBI14'
+    mnem_tfpa = 'DFV22FPATEET'
+    mnem_trxb = 'DRV222RXBAMPT'
+
+    T_RXB = get_val_from_mnem(data, mnem_trxb)
+    T_FPA = get_val_from_mnem(data, mnem_tfpa)
+    RF_bias = get_val_from_mnem(data, mnem_rfb)
+    t = data[3].data['TIME']
+    G_V223 = G(RF_bias, T_RXB, T_FPA, t, pars)
+
+    fig, axes = plt.subplots(sharex=True, nrows=2, ncols=2)
+    axs = axes.flatten()
+    axs[0].plot(t, T_RXB)
+    axs[1].plot(t, T_FPA)
+    axs[2].plot(t, RF_bias)
+    axs[3].plot(t, G_V223)
+
+
+    plt.show()
