b49cc97cbf6d98f46e6b09b6720ccecf6b5410e5
Kristian Joten Andersen <krisjand@astro.uio.no>
2020-05-13 12:36:25 +0200

    small update to alm sampler, now samples up to maximum lmax set by COMP_<paramete>_<poltype_ind>_LMAX

--- src/commander/comm_diffuse_comp_mod.f90
+++ src/commander/comm_diffuse_comp_mod.f90
@@ -149,7 +149,7 @@ contains
     self%nside         = cpar%cs_nside(id_abs)
     self%lmax_amp      = cpar%cs_lmax_amp(id_abs)
     self%l_apod        = cpar%cs_l_apod(id_abs)
-    self%lmax_ind      = cpar%cs_lmax_ind(id_abs) 
+    self%lmax_ind      = cpar%cs_lmax_ind(id_abs) !should add check tos set this to max of lmax_ind and any lmax_ind_pol 
     self%cltype        = cpar%cs_cltype(id_abs)
     self%cg_scale      = cpar%cs_cg_scale(id_abs)
     self%nmaps         = 1; if (self%pol) self%nmaps = 3
