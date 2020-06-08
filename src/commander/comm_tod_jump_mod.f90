21610d8df8400370ce6f9cff4fefb61c08bcff7c
Harald Thommesen <haraltho@propus.uio.no>
2020-06-08 11:54:07 +0200

    Fixed indexing bug in jump_scan.

--- src/commander/comm_tod_jump_mod.f90
+++ src/commander/comm_tod_jump_mod.f90
@@ -9,7 +9,7 @@ module comm_tod_jump_mod
     module procedure tod2file_dp
     module procedure tod2file_int
   end interface tod2file
-  
+
 contains
 
   subroutine tod2file_sp(filename,d)
@@ -28,7 +28,7 @@ contains
     do i = 1, length
       write(unit,*) d(i)
     end do
-    
+
     close(unit)
 
   end subroutine tod2file_sp
@@ -50,7 +50,7 @@ contains
     do i = 1, length
       write(unit,*) d(i)
     end do
-    
+
     close(unit)
   end subroutine tod2file_dp
 
@@ -71,7 +71,7 @@ contains
     do i = 1, length
       write(unit,*) d(i)
     end do
-    
+
     close(unit)
   end subroutine tod2file_int
 
@@ -95,7 +95,7 @@ contains
 
     real(sp), intent(in), dimension(:)   :: x
     real(dp)                             :: mean
-    integer(i4b)                         :: n 
+    integer(i4b)                         :: n
 
     n = size(x)
     if (n==1) then
@@ -111,7 +111,7 @@ contains
   real(dp) function std_nan(x)
     implicit none
     real(sp), intent(in), dimension(:)   :: x
-    integer(i4b)                         :: n 
+    integer(i4b)                         :: n
 
     real(dp)                             :: st_dev
     integer(i4b)                         :: counter, i
@@ -148,7 +148,7 @@ contains
     real(sp),     dimension(:), intent(in)   :: x
     integer(i4b), dimension(:), intent(in)   :: flag
 
-    integer(i4b)                         :: n 
+    integer(i4b)                         :: n
     integer(i4b)                         :: counter, i
     real(sp), allocatable, dimension(:)  :: x_not_flagged
 
@@ -187,7 +187,7 @@ contains
    counter = 0
    n = size(x)
    avg = sum(x)/n
-   
+
    if (isnan(avg)) then
       do i=1, n
          if (.not. isnan(x(i))) then
@@ -293,57 +293,57 @@ contains
     logical                                    :: switch
     real(dp)                                   :: mean_low, mean_high
     real(sp)                                   :: std1, std2, std_mean
- 
+
     ! noise: If "true", then white noise is added ontop of the interpolation
- 
+
     N = 100
     write(*,*) "Routine: Gap fill deprecated"
- 
+
     tod_gapfill = tod
     tod_len = size(tod)
     marker = 0
- 
+
     do i=1, tod_len
        if (i<=marker) cycle
        if (flag(i)==1) then
           counter = 0
           switch = .true.
           do while (switch .and. ((i+counter)<=tod_len))
- 
+
              if ((i+counter==tod_len) .and. (flag(i+counter)==1)) then
-                tod_gapfill(i:counter) = 0 
+                tod_gapfill(i:counter) = 0
              end if
-              
+
              if (flag(i+counter)==0) then
                 if (i==1) then
-                   tod_gapfill(i:counter) = 0 
+                   tod_gapfill(i:counter) = 0
                 else
- 
+
                    mean_low  = mean_flagged(tod_gapfill(i-1-N:i-1),flag(i-1-N:i-1))
                    mean_high = mean_flagged(tod_gapfill(i+counter:i+counter+N),flag(i+counter:i+counter+N))
-                   
+
                    if (noise) then
                       std1 = std_flagged(tod_gapfill(i-1-N:i-1),flag(i-1-N:i-1))
                       std2 = std_flagged(tod_gapfill(i+counter:i+counter+N),flag(i+counter:i+counter+N))
                       std_mean = (std1+std2)/2.0
                       if (isnan(std_mean)) std_mean = 0 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                    end if
-                   
+
                    do j=1, counter
                       tod_gapfill(i+j-1) = mean_low + (j)*(mean_high-mean_low)/(counter+1)
                       if (noise) tod_gapfill(i+j-1) = tod_gapfill(i+j-1) + std_mean*rand_gauss(handle)
-                   end do   
+                   end do
                 end if
                 switch = .false.
                 marker = i+counter-1
              else
                 counter = counter + 1
              end if
-              
+
           end do
        end if
     end do
- 
+
   end subroutine gap_fill_linear_deprecated
 
 
@@ -368,19 +368,18 @@ contains
    tod_gapfill = tod
    tod_len = size(tod)
    counting = .false.
-   do i=1, tod_len
 
+   do i=1, tod_len
       ! If you reach a flagged stretch, start counting and mark position
       if ((flag(i)==1) .and. (.not. counting)) then
          marker = i
          counting = .true.
-         counter = 1 
-         
-         
+         counter = 1
+
       ! When leaving flagged stretch or flagged stretch reaches the end of tod -> do the interpolation
       else if (((flag(i)==0) .or. ((flag(i)==1) .and. (i==tod_len))) .and. counting) then
          counting = .false.
-         
+
          ! Compute mean before and after the gap
          low  = max(1,marker-N)
          high = min(i+N-1,tod_len)
@@ -394,7 +393,7 @@ contains
             mean_low  = mean_flagged(tod_gapfill(low:marker-1),flag(low:marker-1))
             mean_high = mean_flagged(tod_gapfill(i:high),flag(i:high))
          end if
-         
+
          ! Compute std before and after the gap
          if (noise) then
             if (marker==1) then
@@ -403,20 +402,20 @@ contains
             else if (i==tod_len) then
                std_low = std_flagged(tod_gapfill(low:marker-1),flag(low:marker-1))
                std_high = std_low
-            else            
+            else
                std_low  = std_flagged(tod_gapfill(low:marker-1),flag(low:marker-1))
                std_high = std_flagged(tod_gapfill(i:high),flag(i:high))
             end if
             std_combined = (std_low + std_high)/2
             if ((std_low==0) .or. (std_high==0)) std_combined = max(std_low,std_high)
          end if
-         
+
          ! Do the interpolation
          do j=marker, i-1
             tod_gapfill(j) = mean_low + (j-marker+1)*(mean_high-mean_low)/(counter+1)
             if (noise) tod_gapfill(j) = tod_gapfill(j) + std_combined*rand_gauss(handle)
          end do
-         
+
       ! When counting, keep counting
       else if ((flag(i)==1) .and. counting) then
          counter = counter + 1
@@ -434,30 +433,30 @@ contains
     real(sp),    allocatable,     dimension(:),   intent(inout) :: offset_level
     type(planck_rng),             intent(inout) :: handle
     integer(i4b), allocatable,    dimension(:,:), intent(inout) :: jumpflag_range
- 
+
     real(sp), allocatable, dimension(:)        :: tod_gapfill
     real(dp), allocatable, dimension(:)        :: rolling_std
     integer(i4b)                               :: tod_len, N, i, threshold, num_offsets, counter, low, high, N_delta, marker, len_min
     real(dp)                                   :: std_old, mean_old, mean_new, x_new, x_old, st_dev, std_test, med, delta, delta_l, delta_r
     character(len=100)                         :: filename
-    logical                                    :: switch, first_call, counting   
- 
+    logical                                    :: switch, first_call, counting
+
     !write(*,*) 'Routine: Jump scan'
- 
+
     N = 100
     threshold = 2
     tod_len = size(tod)
     jumps(:) = 0
     len_min = 2000
- 
+
     ! Interpolate cosmic ray gaps
     allocate(tod_gapfill(tod_len))
     call gap_fill_linear(tod,flag,tod_gapfill,handle,.false.)
- 
+
     ! Compute rolling standard deviation
     allocate(rolling_std(tod_len))
     rolling_std = 0
-   
+
     do i=N+1, tod_len-N
        if ((i==N+1) .or. (modulo(i,1)==0)) then
           std_old = std(tod_gapfill(i-N:i+N))
@@ -466,16 +465,16 @@ contains
           x_new = tod_gapfill(i+N)
           x_old = tod_gapfill(i-N-1)
           st_dev = welford_std(x_new, x_old, std_old, mean_new, mean_old, 2*N+1)
-         
+
           mean_old = mean_new
           std_old = st_dev
        end if
        rolling_std(i) = std_old
     end do
- 
+
     ! Compute median
     med = median_flagged(rolling_std,flag)
-    
+
     ! Do the flagging
     where (rolling_std > (threshold*med)) jumps = 1
 
@@ -496,7 +495,7 @@ contains
          counter = 0
       end if
     end do
-         
+
 
     ! Find number of offsets so that offset list can be allocated
     num_offsets = 0
@@ -510,11 +509,11 @@ contains
        end if
     end do
     if (jumps(tod_len)==0) num_offsets = num_offsets + 1
-    
+
     allocate(offset_range(num_offsets,2))
     allocate(offset_level(num_offsets))
     if (num_offsets>1) allocate(jumpflag_range(num_offsets-1,2))
-    
+
     ! Define offset regions
     switch = .true.
     first_call = .true.
@@ -533,25 +532,25 @@ contains
        end if
     end do
     if (jumps(tod_len)==0) offset_range(counter,2) = tod_len
- 
+
     ! Compute average and monopole template
     do i=1, num_offsets
        low  = offset_range(i,1)
        high = offset_range(i,2)
        offset_level(i) = sum(tod_gapfill(low:high))/(high-low+1)
- 
+
        ! Compute correction for drift
        if (i==1) then
           delta = 0
        else
-          delta_l = sum(tod_gapfill(low:low+len_min))/(len_min+1) - offset_level(i)
+          delta_l = sum(tod_gapfill(low:low+len_min-1))/len_min - offset_level(i)
           delta = delta_r - delta_l
        end if
- 
+
        offset_level(i) = offset_level(i) - delta
- 
+
        if (i==num_offsets) exit
-       delta_r = sum(tod_gapfill(high-len_min:high))/(len_min+1) - offset_level(i)
+       delta_r = sum(tod_gapfill(high-len_min+1:high))/len_min - offset_level(i)
     end do
 
     ! Add jump regions to full flags
@@ -564,7 +563,7 @@ contains
       end do
    end if
 
-  
+
   end subroutine jump_scan
 
 
@@ -573,22 +572,22 @@ contains
     integer(i4b), dimension(:,:), intent(in)     :: offset_range
     real(sp),     dimension(:),   intent(in)     :: offset_level
     real(sp),     dimension(:),   intent(inout)  :: s_jump
- 
+
     integer(i4b)                                 :: n_offsets, i, i_low, i_high
- 
+
     !write(*,*) "Routine: Expand Offset List"
- 
+
     n_offsets = size(offset_level)
- 
+
     s_jump = 0
- 
+
     do i=1, n_offsets
        i_low  = offset_range(i,1)
        i_high = offset_range(i,2)
 
-       s_jump(i_low:i_high) = s_jump(i_low:i_high) + offset_level(i)      
+       s_jump(i_low:i_high) = s_jump(i_low:i_high) + offset_level(i)
     end do
- 
+
   end subroutine expand_offset_list
 
 
