c79c8b095edc51ee3e4a1cd6e3768473260a244c
Kristian Joten Andersen <krisjand@astro.uio.no>
2020-06-22 11:05:08 +0200

    added functionality to pixelregion sampling. now one can define maximum number of regions to sample (from 0 to total number of pixel regions), thus allowing some regions to be frozen on input value

--- commander3/src/comm_nonlin_mod.f90
+++ commander3/src/comm_nonlin_mod.f90
@@ -647,7 +647,8 @@ contains
                 if (c%lmax_ind_pol(pl,j) < 0) cycle
 
                 if (cpar%almsamp_pixreg) then
-                   call compute_corrlen(regs(:,1:,pl), c%npixreg(pl,j), maxit(pl), c%corrlen(j,pl))
+                   !call compute_corrlen(regs(:,1:,pl), c%npixreg(pl,j), maxit(pl), c%corrlen(j,pl))
+                   call compute_corrlen(regs(:,1:c%pixreg_max_samp,pl), c%pixreg_max_samp(pl,j), maxit(pl), c%corrlen(j,pl))
                 else
                    call compute_corrlen(alms(:,:,pl), nalm_tot, maxit(pl), c%corrlen(j,pl))
                 end if
