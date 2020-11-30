aa10ac0b69e55e394919a81710b0580148cbfc75
Duncan <duncanjosephwatts@gmail.com>
2020-11-30 17:44:25 +0100

    Running a test without any masking, so BiCG-stab should be identical to CG

--- commander3/src/comm_tod_mapmaking_mod.f90
+++ commander3/src/comm_tod_mapmaking_mod.f90
@@ -134,10 +134,8 @@ contains
             ! is 1 (low emission), pixA is updated and pixB isn't.
             f_A = 1-pA*(1-pB)
             f_B = 1-pB*(1-pA)
-            !f_A = pB
-            !f_B = pA
-            !f_A = 1
-            !f_B = 1
+            f_A = 1
+            f_B = 1
 
             do i = 1, nout
                d = data(i, t, det)
@@ -214,10 +212,8 @@ contains
                pB = pmask(rpix)
                f_A = 1-pA*(1-pB)
                f_B = 1-pB*(1-pA)
-               !f_A = pB
-               !f_B = pA
-               !f_A = 1
-               !f_B = 1
+               f_A = 1
+               f_B = 1
                ! This is the model for each timestream
                ! The sgn parameter is +1 for timestreams 13 and 14, -1
                ! for timestreams 23 and 24, and also is used to switch
