Made changes to flag computation and fixed the RIMO file

commander3/todscripts/wmap/cg_solver.py
@@ -193,134 +193,137 @@ def get_data(fname, band, xbar, dxbar, nside=256, pol=False, mask=True):
         b_s = np.zeros(npix)
     labels = [f'{band}13', f'{band}14',f'{band}23',f'{band}24']
     f= h5py.File(fname, 'r')
+    obsids = list(f.keys())
     obsid = str(list(f.keys())[0])
-    huffTree = f[obsid+'/common/hufftree']
-    huffSymb = f[obsid+'/common/huffsymb']
-    h = huffman.Huffman(tree=huffTree, symb=huffSymb)
+    for obsind in range(len(obsids)):
+        obsid = str(obsids[obsind])
+        huffTree = f[obsid+'/common/hufftree']
+        huffSymb = f[obsid+'/common/huffsymb']
+        h = huffman.Huffman(tree=huffTree, symb=huffSymb)
 
 
 
-    DAs = [[], [], [], []]
-    flags = [[], [], [], []]
-    sigmas = []
-    gains = np.zeros(len(labels))
-    npsi = 2048
-    psiBins = np.linspace(0, 2*np.pi, npsi)
-    for num, label in enumerate(labels):
-        TODs = np.array(f[obsid + '/' + label + '/tod'])
-        scalars = f[obsid + '/' + label + '/scalars']
-        gains[num] = scalars[0]
-        flag = h.Decoder(np.array(f[obsid + '/' + label + '/flag']))
-        flags[num] = flags[num] + flag.tolist()
-        DAs[num] = DAs[num] + TODs.tolist()
-        sigmas.append(TODs.std())
-        if label == f'{band}13':
-            pixA = h.Decoder(np.array(f[obsid + '/' + label + \
-                '/pixA'])).astype('int')
-            pixB = h.Decoder(np.array(f[obsid + '/' + label + \
-                '/pixB'])).astype('int')
-            if pol:
-                psiA = psiBins[h.Decoder(np.array(f[obsid + '/' + label + \
-                    '/psiA'])).astype('int')]
-                psiB = psiBins[h.Decoder(np.array(f[obsid + '/' + label + \
-                    '/psiB'])).astype('int')]
-    # bit array; bit 0 means data is suspect, bit 12 means Mars in AB, etc. til
-    # 10. I think I'll just try to get rid of all data where there are any
-    # planets in the beam, although i think the official release tried to use as
-    # much data as possible, i.e., only cutting out the center.
-    flags = np.array(flags).sum(axis=0)
-    #flags *= 0
-    inds = (flags == 0)
-    #inds = np.isfinite(flags)
+        DAs = [[], [], [], []]
+        flags = [[], [], [], []]
+        sigmas = []
+        gains = np.zeros(len(labels))
+        npsi = 2048
+        psiBins = np.linspace(0, 2*np.pi, npsi)
+        for num, label in enumerate(labels):
+            TODs = np.array(f[obsid + '/' + label + '/tod'])
+            scalars = f[obsid + '/' + label + '/scalars']
+            gains[num] = scalars[0]
+            flag = h.Decoder(np.array(f[obsid + '/' + label + '/flag']))
+            flags[num] = flags[num] + flag.tolist()
+            DAs[num] = DAs[num] + TODs.tolist()
+            sigmas.append(TODs.std())
+            if label == f'{band}13':
+                pixA = h.Decoder(np.array(f[obsid + '/' + label + \
+                    '/pixA'])).astype('int')
+                pixB = h.Decoder(np.array(f[obsid + '/' + label + \
+                    '/pixB'])).astype('int')
+                if pol:
+                    psiA = psiBins[h.Decoder(np.array(f[obsid + '/' + label + \
+                        '/psiA'])).astype('int')]
+                    psiB = psiBins[h.Decoder(np.array(f[obsid + '/' + label + \
+                        '/psiB'])).astype('int')]
+        # bit array; bit 0 means data is suspect, bit 12 means Mars in AB, etc. til
+        # 10. I think I'll just try to get rid of all data where there are any
+        # planets in the beam, although i think the official release tried to use as
+        # much data as possible, i.e., only cutting out the center.
+        flags = np.array(flags).sum(axis=0)
+        #flags *= 0
+        inds = (flags == 0)
+        #inds = np.isfinite(flags)
 
-    DAs = np.array(DAs)/gains.reshape(4,1)
-    #sigma0 = np.mean(np.array(sigmas)**2)**0.5
-
-
-    
-    d1 = 0.5*(DAs[0] + DAs[1])
-    d2 = 0.5*(DAs[2] + DAs[3])
-
-    n1 = 0.5*(DAs[0] - DAs[1])
-    n2 = 0.5*(DAs[2] - DAs[3])
-
-    n = 0.5*(n1+n2)
-    sigma0 = n.std()
-    
-    d = 0.5*(d1 + d2) # = i_A - i_B
-    p = 0.5*(d1 - d2) # = q_A*cos(2*g_A) + u_A*sin(2*g_A) - q_B*cos(2*g_B) - u_B*sin(2*g_B)
-
-    N_tod = len(d)
-    for i in range(25):
-        d[i*N_tod:(i+1)*N_tod] -= np.median(d[i*N_tod:(i+1)*N_tod])
-        p[i*N_tod:(i+1)*N_tod] -= np.median(p[i*N_tod:(i+1)*N_tod])
+        DAs = np.array(DAs)/gains.reshape(4,1)
+        #sigma0 = np.mean(np.array(sigmas)**2)**0.5
 
 
-    # subtract dipole solution from d
-    #   d = d - ((1+xbar)*dipole[pixA] - (1-xbar)*dipole[pixB])
+        
+        d1 = 0.5*(DAs[0] + DAs[1])
+        d2 = 0.5*(DAs[2] + DAs[3])
 
-    #   p = p - dxbar*(dipole[pixA] + dipole[pixB])
+        n1 = 0.5*(DAs[0] - DAs[1])
+        n2 = 0.5*(DAs[2] - DAs[3])
 
-    # subtract orbital dipole
-    vsun = f[obsid + '/common/vsun'][:] # km/s
-    c = 299792.458
-    beta = vsun/c
-    vecs = np.array(hp.pix2vec(nside, np.arange(hp.nside2npix(nside))))
-    gamma = 1/np.sqrt(1-sum(beta**2))
-    # in mK
-    orb_dip = 2.7275e3*(1/(1-beta.dot(vecs))/gamma-1)
+        n = 0.5*(n1+n2)
+        sigma0 = n.std()
+        
+        d = 0.5*(d1 + d2) # = i_A - i_B
+        p = 0.5*(d1 - d2) # = q_A*cos(2*g_A) + u_A*sin(2*g_A) - q_B*cos(2*g_B) - u_B*sin(2*g_B)
 
-    d = d - ((1+xbar)*orb_dip[pixA] - (1-xbar)*orb_dip[pixB])
-    p = p - dxbar*(orb_dip[pixA] + orb_dip[pixB])
-    
-
-    # most aggressive mask
-    if mask:
-        pm = hp.read_map('analysis_masks/wmap_processing_mask_K_yr2_r9_9yr_v5.fits',
-                verbose=False, dtype=None)
-        pm = hp.ud_grade(pm, nside)
-    else:
-        pm = np.ones(npix)
-
-    pA = pm[pixA]
-    pB = pm[pixB]
-    # This SHOULD make it so that if pA is 0 (high emission) and pB is 1 (low
-    # emission), pixA is updated and pixB isn't.
-    f_A = (1 - pA*(1-pB))*inds
-    f_B = (1 - pB*(1-pA))*inds
-
-    # Let's make it a bit easier, if horn A is in a high-emission region, horn B
-    # isn't updated.
-    #f_A = pB*inds
-    #f_B = pA*inds
+        N_tod = len(d)
+        for i in range(25):
+            d[i*N_tod:(i+1)*N_tod] -= np.median(d[i*N_tod:(i+1)*N_tod])
+            p[i*N_tod:(i+1)*N_tod] -= np.median(p[i*N_tod:(i+1)*N_tod])
 
 
-    sigmas = np.ones(len(d))*sigma0
-    for t in range(len(d)):
-        '''
-        Asymmetric masking means that when one beam is in a high Galactic
-        emission region (as determined by a processing mask; Limon et al. 2010)
-        and the other beam is in a low Galactic emission region, only the pixel
-        in the high emission region is iteratively updated.
-        # pm == 0 means it is in a high emission region
-        '''
-        b[pixA[t]]   += f_A[t]*((1+xbar)*d[t] + dxbar*p[t])/sigma0**2
-        b[pixB[t]]   -= f_B[t]*((1-xbar)*d[t] - dxbar*p[t])/sigma0**2
-        b_q[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])*np.cos(2*psiA[t])/sigma0**2
-        b_q[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])*np.cos(2*psiB[t])/sigma0**2
-        b_u[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])*np.sin(2*psiA[t])/sigma0**2
-        b_u[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])*np.sin(2*psiB[t])/sigma0**2
-        b_s[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])/sigma0**2
-        b_s[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])/sigma0**2
+        # subtract dipole solution from d
+        #   d = d - ((1+xbar)*dipole[pixA] - (1-xbar)*dipole[pixB])
 
-        M[pixA[t]]   += f_A[t]*(1+xbar+dxbar)**2
-        M[pixB[t]]   += f_B[t]*(1-xbar-dxbar)**2
-        M_q[pixA[t]] += f_A[t]*((1+xbar+dxbar)*np.cos(2*psiA[t]))**2
-        M_q[pixB[t]] += f_B[t]*((1-xbar-dxbar)*np.cos(2*psiB[t]))**2
-        M_u[pixA[t]] += f_A[t]*((1+xbar+dxbar)*np.sin(2*psiA[t]))**2
-        M_u[pixB[t]] += f_B[t]*((1-xbar-dxbar)*np.sin(2*psiB[t]))**2
-        M_s[pixA[t]] += f_A[t]*(1+xbar+dxbar)**2
-        M_s[pixB[t]] += f_B[t]*(1-xbar-dxbar)**2
+        #   p = p - dxbar*(dipole[pixA] + dipole[pixB])
+
+        # subtract orbital dipole
+        vsun = f[obsid + '/common/vsun'][:] # km/s
+        c = 299792.458
+        beta = vsun/c
+        vecs = np.array(hp.pix2vec(nside, np.arange(hp.nside2npix(nside))))
+        gamma = 1/np.sqrt(1-sum(beta**2))
+        # in mK
+        orb_dip = 2.7275e3*(1/(1-beta.dot(vecs))/gamma-1)
+
+        d = d - ((1+xbar)*orb_dip[pixA] - (1-xbar)*orb_dip[pixB])
+        p = p - dxbar*(orb_dip[pixA] + orb_dip[pixB])
+        
+
+        # most aggressive mask
+        if mask:
+            pm = hp.read_map('analysis_masks/wmap_processing_mask_K_yr2_r9_9yr_v5.fits',
+                    verbose=False, dtype=None)
+            pm = hp.ud_grade(pm, nside)
+        else:
+            pm = np.ones(npix)
+
+        pA = pm[pixA]
+        pB = pm[pixB]
+        # This SHOULD make it so that if pA is 0 (high emission) and pB is 1 (low
+        # emission), pixA is updated and pixB isn't.
+        f_A = (1 - pA*(1-pB))*inds
+        f_B = (1 - pB*(1-pA))*inds
+
+        # Let's make it a bit easier, if horn A is in a high-emission region, horn B
+        # isn't updated.
+        #f_A = pB*inds
+        #f_B = pA*inds
+
+
+        sigmas = np.ones(len(d))*sigma0
+        for t in range(len(d)):
+            '''
+            Asymmetric masking means that when one beam is in a high Galactic
+            emission region (as determined by a processing mask; Limon et al. 2010)
+            and the other beam is in a low Galactic emission region, only the pixel
+            in the high emission region is iteratively updated.
+            # pm == 0 means it is in a high emission region
+            '''
+            b[pixA[t]]   += f_A[t]*((1+xbar)*d[t] + dxbar*p[t])/sigma0**2
+            b[pixB[t]]   -= f_B[t]*((1-xbar)*d[t] - dxbar*p[t])/sigma0**2
+            b_q[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])*np.cos(2*psiA[t])/sigma0**2
+            b_q[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])*np.cos(2*psiB[t])/sigma0**2
+            b_u[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])*np.sin(2*psiA[t])/sigma0**2
+            b_u[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])*np.sin(2*psiB[t])/sigma0**2
+            b_s[pixA[t]] += f_A[t]*((1+xbar)*p[t] + dxbar*d[t])/sigma0**2
+            b_s[pixB[t]] -= f_B[t]*((1-xbar)*p[t] - dxbar*d[t])/sigma0**2
+
+            M[pixA[t]]   += f_A[t]*(1+xbar+dxbar)**2
+            M[pixB[t]]   += f_B[t]*(1-xbar-dxbar)**2
+            M_q[pixA[t]] += f_A[t]*((1+xbar+dxbar)*np.cos(2*psiA[t]))**2
+            M_q[pixB[t]] += f_B[t]*((1-xbar-dxbar)*np.cos(2*psiB[t]))**2
+            M_u[pixA[t]] += f_A[t]*((1+xbar+dxbar)*np.sin(2*psiA[t]))**2
+            M_u[pixB[t]] += f_B[t]*((1-xbar-dxbar)*np.sin(2*psiB[t]))**2
+            M_s[pixA[t]] += f_A[t]*(1+xbar+dxbar)**2
+            M_s[pixB[t]] += f_B[t]*(1-xbar-dxbar)**2
 
     b_p = np.concatenate((b_q, b_u, b_s))
     M_p = np.concatenate((M_q, M_u, M_s))
@@ -967,12 +970,11 @@ if __name__ == '__main__':
     for b in ['K1']:
         get_cg(band=b, nfiles=512, sparse_test=False, sparse_only=True, 
                 imbalance=True, mask=True, pol=True, imax=1000, nside=512)
-        #plot_maps_pol(band=b, nside=512, version=16)
-        #plot_maps_pol(band=b, nside=512, version=14)
+    #    #plot_maps_pol(band=b, nside=512, version=16)
+    #    #plot_maps_pol(band=b, nside=512, version=14)
     #get_cg(band='Ka1', nfiles=400, sparse_test=False, sparse_only=True,
     #        processing_mask=False)
     #get_cg(band='Q1', nfiles=100, sparse_test=False, sparse_only=True)
     #for band in bands:
     #    get_cg(band=band, nfiles=200, sparse_test=False, sparse_only=True)
     #get_cg(band='V1')
-    #check_hdf5(version=11)

commander3/todscripts/wmap/make_wmap_rimo.py
@@ -46,8 +46,8 @@ import reproject
 
 
 
-fname_out = '/mn/stornext/d16/cmbco/bp/dwatts/wmap/data/WMAP_instrument_v2.h5'
-fname_out = 'data/test.h5'
+fname_out = '/mn/stornext/d16/cmbco/bp/dwatts/WMAP/data_WMAP/WMAP_instrument_v2.h5'
+#fname_out = 'data/test.h5'
 labels = ['K', 'Ka', 'Q', 'V', 'W']
 
 
@@ -117,168 +117,168 @@ with h5py.File(fname_out, 'a') as f:
 
 
 
-#
-#    fnames = glob('data/wmap_hybrid_beam_maps_*_9yr_v5.fits')
-#    fnames.sort()
-#    
-#    
-#    hdus = [fits.open(fname)[0] for fname in fnames]
-#    wcs = WCS(hdus[0].header) # hdus are the same
-#    
-#    
-#    # should be at least 1024, since pixel size is 0.04 ~ 58.6/nside
-#    res_min = np.log(58.6/0.04)/np.log(2)
-#    nside_min = 2**np.ceil(res_min)
-#    nside_beam = 1024
-#    
-#    nside_beam = 2048
-#    
-#    
-#    lmax = 2400
-#    mmax = 100
-#    
-#    
-#    # I am nearly certain that the projection that they use, 
-#    # X=2*sin(theta/2)*cos(phi), is zenithal equal area, making the coordinate
-#    # system centered at the north pole.
-#    target_header = fits.Header.fromstring("""
-#    NAXIS   =                    2
-#    NAXIS1  =                  600
-#    NAXIS2  =                  600
-#    CTYPE1  = 'GLON-ZEA'
-#    CRPIX1  =                0.5
-#    CRVAL1  =                -11.98
-#    CDELT1  =               0.04
-#    CUNIT1  = 'deg     '
-#    CTYPE2  = 'GLAT-ZEA'
-#    CRPIX2  =                0.5
-#    CRVAL2  =                -11.98
-#    CDELT2  =                0.04
-#    CUNIT2  = 'deg     '
-#    COORDSYS= 'icrs    '
-#    """, sep='\n')
-#    
-#    for fname in fnames:
-#        data = fits.open(fname)
-#        
-#        m_A, footprint_A = reproject.reproject_to_healpix((data[0].data[0], target_header), 'G', 
-#                                                       nside=nside_beam,
-#                                                       order=3)
-#    
-#        #hp.mollview(m_A)
-#        #plt.show()
-#        m_B, footprint_B = reproject.reproject_to_healpix((data[0].data[2], target_header), 'G', 
-#                                                       nside=nside_beam,
-#                                                       order=3)
-#        m_A[footprint_A==0] = 0
-#        m_B[footprint_B==0] = 0
-#    
-#        r = hp.rotator.Rotator(rot=hp.pix2ang(nside_beam, np.where(m_A==m_A.max())[0][0],
-#            lonlat=True))
-#        m_A_rotated = r.rotate_map_pixel(m_A)
-#        r2 = hp.rotator.Rotator(rot=(0,-90,0))
-#        m_A_rotated = r2.rotate_map_pixel(m_A_rotated)
-#    
-#        alm_A = hp.map2alm(m_A_rotated, lmax=lmax)
-#    
-#        b_lm_A = np.zeros((lmax+1)**2)
-#        for i in range(len(alm_A)):
-#            li, mi = hp.sphtfunc.Alm.getlm(lmax,i=i)
-#            if (li <= lmax) & (mi <= mmax):
-#                ind_real = li**2 + li + mi
-#                ind_imag = li**2 + li - mi
-#                b_lm_A[ind_real] = alm_A[i].real
-#                if mi != 0:
-#                    b_lm_A[ind_imag] = alm_A[i].imag
-#                   
-#    
-#    
-#    
-#    
-#    
-#        r = hp.rotator.Rotator(rot=hp.pix2ang(nside_beam, np.where(m_B==m_B.max())[0][0],
-#            lonlat=True))
-#        m_B_rotated = r.rotate_map_pixel(m_B)
-#        r2 = hp.rotator.Rotator(rot=(0,-90,0))
-#        m_B_rotated = r2.rotate_map_pixel(m_B_rotated)
-#    
-#        alm_B = hp.map2alm(m_B_rotated)
-#        b_lm_B = np.zeros((lmax+1)**2)
-#        for i in range(len(alm_B)):
-#            li, mi = hp.sphtfunc.Alm.getlm(lmax,i=i)
-#            if (li <= lmax) & (mi <= mmax):
-#                ind_real = li**2 + li + mi
-#                ind_imag = li**2 + li - mi
-#                b_lm_B[ind_real] = alm_B[i].real
-#                if mi != 0:
-#                    b_lm_B[ind_imag] = alm_B[i].imag
-#    
-#        DA = fname.split('_')[4]
-#    
-#        with h5py.File(fname_out, 'a') as f:
-#            f.create_dataset(DA + '13/beam/T', data=b_lm_A)
-#            f.create_dataset(DA + '23/beam/T', data=b_lm_A)
-#            f.create_dataset(DA + '14/beam/T', data=b_lm_B)
-#            f.create_dataset(DA + '24/beam/T', data=b_lm_B)
-#            f.create_dataset(DA + '13/beamlmax', data=[lmax])
-#            f.create_dataset(DA + '23/beamlmax', data=[lmax])
-#            f.create_dataset(DA + '14/beamlmax', data=[lmax])
-#            f.create_dataset(DA + '24/beamlmax', data=[lmax])
-#            f.create_dataset(DA + '13/beammmax', data=[mmax])
-#            f.create_dataset(DA + '23/beammmax', data=[mmax])
-#            f.create_dataset(DA + '14/beammmax', data=[mmax])
-#            f.create_dataset(DA + '24/beammmax', data=[mmax])
-#            
-#            f.create_dataset(DA + '13/sl/T', data=b_lm_A*0)
-#            f.create_dataset(DA + '23/sl/T', data=b_lm_A*0)
-#            f.create_dataset(DA + '14/sl/T', data=b_lm_B*0)
-#            f.create_dataset(DA + '24/sl/T', data=b_lm_B*0)
-#            f.create_dataset(DA + '13/sllmax', data=[lmax])
-#            f.create_dataset(DA + '23/sllmax', data=[lmax])
-#            f.create_dataset(DA + '14/sllmax', data=[lmax])
-#            f.create_dataset(DA + '24/sllmax', data=[lmax])
-#            f.create_dataset(DA + '13/slmmax', data=[mmax])
-#            f.create_dataset(DA + '23/slmmax', data=[mmax])
-#            f.create_dataset(DA + '14/slmmax', data=[mmax])
-#            f.create_dataset(DA + '24/slmmax', data=[mmax])
-#    
-#    
-#            f.create_dataset(DA + '13/mbeam_eff', data=[1])
-#            f.create_dataset(DA + '23/mbeam_eff', data=[1])
-#            f.create_dataset(DA + '14/mbeam_eff', data=[1])
-#            f.create_dataset(DA + '24/mbeam_eff', data=[1])
-#            f.create_dataset(DA + '13/psi_ell', data=[0])
-#            f.create_dataset(DA + '23/psi_ell', data=[0])
-#            f.create_dataset(DA + '14/psi_ell', data=[0])
-#            f.create_dataset(DA + '24/psi_ell', data=[0])
-#    
-#    
-#    # a_lm indices
-#    #data = h5py.File('/mn/stornext/d16/cmbco/bp/dwatts/wmap/data/LFI_instrument_v4.h5', 'r')
-#    #lmax = data['18M/beamlmax'][...][0]
-#    #T_lm = data['18M/beam/T'][...]
-#    #T_lmcpx = np.zeros(hp.sphtfunc.Alm.getidx(lmax,lmax,lmax)+1, dtype='complex')
-#    #inds = {}
-#    #for li in range(lmax+1):
-#    #    for mi in range(-li, li+1):
-#    #        inds[li**2+li+mi] = (li,mi)
-#    #for i in range(len(inds)):
-#    #    li, mi = inds[i]
-#    #    if mi >= 0:
-#    #        idx = hp.sphtfunc.Alm.getidx(lmax, li, mi)
-#    #        T_lmcpx[idx] += T_lm[i]
-#    #    else:
-#    #        idx = hp.sphtfunc.Alm.getidx(lmax, li, -mi)
-#    #        T_lmcpx[idx] += 1j*T_lm[i]
-#    
-#    #plt.show()
-#    """
-#    # far sidelobes
-#    fnames = glob('data/wmap_sidelobe_map_*_9yr_v5.fits')
-#    nside = 2**7
-#    inds = hp.nest2ring(nside, np.arange(12*nside**2))
-#    for fname in fnames:
-#        m = fits.open(fname)
-#        # NB; these are in nest format
-#        T = m[1].data['TEMPERATURE'][inds]
-#    """
+
+    fnames = glob('data/wmap_hybrid_beam_maps_*_9yr_v5.fits')
+    fnames.sort()
+    
+    
+    hdus = [fits.open(fname)[0] for fname in fnames]
+    wcs = WCS(hdus[0].header) # hdus are the same
+    
+    
+    # should be at least 1024, since pixel size is 0.04 ~ 58.6/nside
+    res_min = np.log(58.6/0.04)/np.log(2)
+    nside_min = 2**np.ceil(res_min)
+    nside_beam = 1024
+    
+    nside_beam = 2048
+    
+    
+    lmax = 2400
+    mmax = 100
+    
+    
+    # I am nearly certain that the projection that they use, 
+    # X=2*sin(theta/2)*cos(phi), is zenithal equal area, making the coordinate
+    # system centered at the north pole.
+    target_header = fits.Header.fromstring("""
+    NAXIS   =                    2
+    NAXIS1  =                  600
+    NAXIS2  =                  600
+    CTYPE1  = 'GLON-ZEA'
+    CRPIX1  =                0.5
+    CRVAL1  =                -11.98
+    CDELT1  =               0.04
+    CUNIT1  = 'deg     '
+    CTYPE2  = 'GLAT-ZEA'
+    CRPIX2  =                0.5
+    CRVAL2  =                -11.98
+    CDELT2  =                0.04
+    CUNIT2  = 'deg     '
+    COORDSYS= 'icrs    '
+    """, sep='\n')
+    
+    for fname in fnames:
+        data = fits.open(fname)
+        
+        #m_A, footprint_A = reproject.reproject_to_healpix((data[0].data[0], target_header), 'G', 
+        #                                               nside=nside_beam,
+        #                                               order=3)
+    
+        ##hp.mollview(m_A)
+        ##plt.show()
+        #m_B, footprint_B = reproject.reproject_to_healpix((data[0].data[2], target_header), 'G', 
+        #                                               nside=nside_beam,
+        #                                               order=3)
+        #m_A[footprint_A==0] = 0
+        #m_B[footprint_B==0] = 0
+    
+        #r = hp.rotator.Rotator(rot=hp.pix2ang(nside_beam, np.where(m_A==m_A.max())[0][0],
+        #    lonlat=True))
+        #m_A_rotated = r.rotate_map_pixel(m_A)
+        #r2 = hp.rotator.Rotator(rot=(0,-90,0))
+        #m_A_rotated = r2.rotate_map_pixel(m_A_rotated)
+    
+        #alm_A = hp.map2alm(m_A_rotated, lmax=lmax)
+    
+        b_lm_A = np.zeros((lmax+1)**2) + 1
+        #for i in range(len(alm_A)):
+        #    li, mi = hp.sphtfunc.Alm.getlm(lmax,i=i)
+        #    if (li <= lmax) & (mi <= mmax):
+        #        ind_real = li**2 + li + mi
+        #        ind_imag = li**2 + li - mi
+        #        b_lm_A[ind_real] = alm_A[i].real
+        #        if mi != 0:
+        #            b_lm_A[ind_imag] = alm_A[i].imag
+        #           
+    
+    
+    
+    
+    
+        #r = hp.rotator.Rotator(rot=hp.pix2ang(nside_beam, np.where(m_B==m_B.max())[0][0],
+        #    lonlat=True))
+        #m_B_rotated = r.rotate_map_pixel(m_B)
+        #r2 = hp.rotator.Rotator(rot=(0,-90,0))
+        #m_B_rotated = r2.rotate_map_pixel(m_B_rotated)
+    
+        #alm_B = hp.map2alm(m_B_rotated)
+        b_lm_B = np.zeros((lmax+1)**2) + 1
+        #for i in range(len(alm_B)):
+        #    li, mi = hp.sphtfunc.Alm.getlm(lmax,i=i)
+        #    if (li <= lmax) & (mi <= mmax):
+        #        ind_real = li**2 + li + mi
+        #        ind_imag = li**2 + li - mi
+        #        b_lm_B[ind_real] = alm_B[i].real
+        #        if mi != 0:
+        #            b_lm_B[ind_imag] = alm_B[i].imag
+    
+        DA = fname.split('_')[4]
+    
+        with h5py.File(fname_out, 'a') as f:
+            f.create_dataset(DA + '13/beam/T', data=b_lm_A)
+            f.create_dataset(DA + '23/beam/T', data=b_lm_A)
+            f.create_dataset(DA + '14/beam/T', data=b_lm_B)
+            f.create_dataset(DA + '24/beam/T', data=b_lm_B)
+            f.create_dataset(DA + '13/beamlmax', data=[lmax])
+            f.create_dataset(DA + '23/beamlmax', data=[lmax])
+            f.create_dataset(DA + '14/beamlmax', data=[lmax])
+            f.create_dataset(DA + '24/beamlmax', data=[lmax])
+            f.create_dataset(DA + '13/beammmax', data=[mmax])
+            f.create_dataset(DA + '23/beammmax', data=[mmax])
+            f.create_dataset(DA + '14/beammmax', data=[mmax])
+            f.create_dataset(DA + '24/beammmax', data=[mmax])
+            
+            f.create_dataset(DA + '13/sl/T', data=b_lm_A*0)
+            f.create_dataset(DA + '23/sl/T', data=b_lm_A*0)
+            f.create_dataset(DA + '14/sl/T', data=b_lm_B*0)
+            f.create_dataset(DA + '24/sl/T', data=b_lm_B*0)
+            f.create_dataset(DA + '13/sllmax', data=[lmax])
+            f.create_dataset(DA + '23/sllmax', data=[lmax])
+            f.create_dataset(DA + '14/sllmax', data=[lmax])
+            f.create_dataset(DA + '24/sllmax', data=[lmax])
+            f.create_dataset(DA + '13/slmmax', data=[mmax])
+            f.create_dataset(DA + '23/slmmax', data=[mmax])
+            f.create_dataset(DA + '14/slmmax', data=[mmax])
+            f.create_dataset(DA + '24/slmmax', data=[mmax])
+    
+    
+            f.create_dataset(DA + '13/mbeam_eff', data=[1])
+            f.create_dataset(DA + '23/mbeam_eff', data=[1])
+            f.create_dataset(DA + '14/mbeam_eff', data=[1])
+            f.create_dataset(DA + '24/mbeam_eff', data=[1])
+            f.create_dataset(DA + '13/psi_ell', data=[0])
+            f.create_dataset(DA + '23/psi_ell', data=[0])
+            f.create_dataset(DA + '14/psi_ell', data=[0])
+            f.create_dataset(DA + '24/psi_ell', data=[0])
+    
+    
+    # a_lm indices
+    #data = h5py.File('/mn/stornext/d16/cmbco/bp/dwatts/wmap/data/LFI_instrument_v4.h5', 'r')
+    #lmax = data['18M/beamlmax'][...][0]
+    #T_lm = data['18M/beam/T'][...]
+    #T_lmcpx = np.zeros(hp.sphtfunc.Alm.getidx(lmax,lmax,lmax)+1, dtype='complex')
+    #inds = {}
+    #for li in range(lmax+1):
+    #    for mi in range(-li, li+1):
+    #        inds[li**2+li+mi] = (li,mi)
+    #for i in range(len(inds)):
+    #    li, mi = inds[i]
+    #    if mi >= 0:
+    #        idx = hp.sphtfunc.Alm.getidx(lmax, li, mi)
+    #        T_lmcpx[idx] += T_lm[i]
+    #    else:
+    #        idx = hp.sphtfunc.Alm.getidx(lmax, li, -mi)
+    #        T_lmcpx[idx] += 1j*T_lm[i]
+    
+    #plt.show()
+    """
+    # far sidelobes
+    fnames = glob('data/wmap_sidelobe_map_*_9yr_v5.fits')
+    nside = 2**7
+    inds = hp.nest2ring(nside, np.arange(12*nside**2))
+    for fname in fnames:
+        m = fits.open(fname)
+        # NB; these are in nest format
+        T = m[1].data['TEMPERATURE'][inds]
+    """