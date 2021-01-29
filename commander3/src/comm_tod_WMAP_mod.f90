276bc8d8a18da1baf2313d9d52b58f9eede347f5
Duncan <duncanjosephwatts@gmail.com>
2021-01-29 21:05:37 +0100

    Added the bandpass contribution

--- commander3/src/comm_tod_WMAP_mod.f90
+++ commander3/src/comm_tod_WMAP_mod.f90
@@ -430,6 +430,8 @@ contains
             do j = 1, ndet
                s_sky(:, j) = (1+self%x_im((j+1)/2))*s_skyA(:,j) - &
                            & (1-self%x_im((j+1)/2))*s_skyB(:,j)
+               s_bp(:, j)  = (1+self%x_im((j+1)/2))*s_bpA(:,j) - &
+                           & (1-self%x_im((j+1)/2))*s_bpB(:,j)
             end do
 
 
@@ -489,8 +491,8 @@ contains
             s_tot = 0
             do j = 1, ndet
                if (.not. self%scans(i)%d(j)%accept) cycle
-               s_totA(:, j) = s_skyA(:, j) + s_slA(:, j) + s_orbA(:,j)
-               s_totB(:, j) = s_skyB(:, j) + s_slB(:, j) + s_orbB(:,j)
+               s_totA(:, j) = s_skyA(:, j) + s_slA(:, j) + s_orbA(:,j) + s_bpA(:,j)
+               s_totB(:, j) = s_skyB(:, j) + s_slB(:, j) + s_orbB(:,j) + s_bpB(:,j)
                s_tot(:, j) = (1+self%x_im((j+1)/2))*s_totA(:,j) - &
                            & (1-self%x_im((j+1)/2))*s_totB(:,j)
             end do
