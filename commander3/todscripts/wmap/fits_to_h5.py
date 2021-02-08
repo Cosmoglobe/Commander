4f33d83169b3fa90155f14d0265fece807168acd
Duncan <duncanjosephwatts@gmail.com>
2021-02-08 19:03:42 +0100

    Added some profiling to fits_to_h5

--- commander3/todscripts/wmap/fits_to_h5.py
+++ commander3/todscripts/wmap/fits_to_h5.py
@@ -77,6 +77,61 @@ fknees *= 1e-3
 from time import sleep
 from time import time as timer
 
+# from https://towardsdatascience.com/how-to-profile-your-code-in-python-e70c834fad89
+import cProfile
+import pstats
+from functools import wraps
+
+
+def profile(output_file=None, sort_by='cumulative', lines_to_print=None, strip_dirs=False):
+    """A time profiler decorator.
+    Inspired by and modified the profile decorator of Giampaolo Rodola:
+    http://code.activestate.com/recipes/577817-profile-decorator/
+    Args:
+        output_file: str or None. Default is None
+            Path of the output file. If only name of the file is given, it's
+            saved in the current directory.
+            If it's None, the name of the decorated function is used.
+        sort_by: str or SortKey enum or tuple/list of str/SortKey enum
+            Sorting criteria for the Stats object.
+            For a list of valid string and SortKey refer to:
+            https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
+        lines_to_print: int or None
+            Number of lines to print. Default (None) is for all the lines.
+            This is useful in reducing the size of the printout, especially
+            that sorting by 'cumulative', the time consuming operations
+            are printed toward the top of the file.
+        strip_dirs: bool
+            Whether to remove the leading path info from file names.
+            This is also useful in reducing the size of the printout
+    Returns:
+        Profile of the decorated function
+    """
+
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _output_file = output_file or func.__name__ + '.prof'
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+            pr.dump_stats(_output_file)
+
+            with open(_output_file, 'w') as f:
+                ps = pstats.Stats(pr, stream=f)
+                if strip_dirs:
+                    ps.strip_dirs()
+                if isinstance(sort_by, (tuple, list)):
+                    ps.sort_stats(*sort_by)
+                else:
+                    ps.sort_stats(sort_by)
+                ps.print_stats(lines_to_print)
+            return retval
+
+        return wrapper
+
+    return inner
 
 from get_gain_model import get_gain
 
@@ -261,11 +316,13 @@ def write_file_parallel(file_ind, i, obsid, obs_ind, daflags, TODs, gain_guesses
     h.GenerateCode(pixArray)
 
 
-    h_Tod = huffman.Huffman("", nside)
-    h_Tod.GenerateCode(todArray)
+    if compress:
+      h_Tod = huffman.Huffman("", nside)
+      h_Tod.GenerateCode(todArray)
 
     huffarray = np.append(np.append(np.array(h.node_max), h.left_nodes), h.right_nodes)
-    huffarray_Tod = np.append(np.append(np.array(h_Tod.node_max), h_Tod.left_nodes), h_Tod.right_nodes)
+    if compress:
+      huffarray_Tod = np.append(np.append(np.array(h_Tod.node_max), h_Tod.left_nodes), h_Tod.right_nodes)
 
 
     #with h5py.File(file_out, 'a') as f:
@@ -754,6 +811,7 @@ def get_psi_multiprocessing_2(i):
 def ang2pix_multiprocessing(nside, theta, phi):
     return hp.ang2pix(nside, theta, phi)
 
+@profile(sort_by='cumulative', strip_dirs=True)
 def fits_to_h5(file_input, file_ind, compress, plot, version, center):
     f_name = file_input.split('/')[-1][:-8]
 
@@ -910,6 +968,8 @@ def main(par=True, plot=False, compress=False, nfiles=sys.maxsize, version=18,
         files = glob(prefix + 'uncalibrated/*.fits')
     files.sort()
     inds = np.arange(len(files))
+    inds = inds[:64]
+    files = np.array(files)[:64]
     #inds = inds[:len(files)//4]
     #files = np.array(files)[:len(files)//4]
     #inds = inds[len(files)//4:2*len(files)//4]
@@ -936,10 +996,12 @@ def main(par=True, plot=False, compress=False, nfiles=sys.maxsize, version=18,
         for i, f in zip(inds, files):
             fits_to_h5(f,i,compress, plot, version, center)
 
+
 if __name__ == '__main__':
     #main(par=True, plot=False, compress=True, version=31, center=True)
     #main(par=True, plot=False, compress=True, version='cal', center=True)
     #main(par=True, plot=False, compress=True, version=34, center=True)
     #main(par=True, plot=False, compress=True, version=35, center=True)
-    main(par=True, plot=False, compress=True, version=36, center=True)
-    test_flags()
+    #main(par=True, plot=False, compress=True, version=36, center=True)
+    main(par=True, plot=False, compress=True, version='test', center=True)
+    #test_flags()
