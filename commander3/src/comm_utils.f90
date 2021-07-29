b47c16668e815fd3060dadff0a1773d934c626b6
Kristian Joten Andersen <krisjand@astro.uio.no>
2021-07-29 10:35:17 +0200

    Moved correlation routines to comm_utils and removed subroutines that are obsolete from comm_nonlin_mod and comm_diffuse_mod

--- commander3/src/comm_utils.f90
+++ commander3/src/comm_utils.f90
@@ -1234,5 +1234,107 @@ contains
       str = adjustl(str)
   end function str
 
+
+  function calc_corr_coeff(spec_arr, n_samp)
+    implicit none
+    integer(i4b),               intent(in), optional :: n_samp
+    real(dp),     dimension(:), intent(in)           :: spec_arr
+    real(dp)                                         :: calc_corr_coeff
+
+    integer(i4b) :: i, j, maxcorr, ns
+    real(dp)     :: x_mean, y_mean, sig_x, sig_y, covarr
+    real(dp), dimension(:), allocatable :: xarr, yarr
+    !
+    !  Function to calculate the Pearson correlation coefficient between samples in a set of a sampled spectral parameter.
+    !
+    !  Arguments:
+    !  ------------------
+    !  spec_arr: double precision array, unknown length
+    !     An array containing the sampled spectral parameters
+    !  n_spec: integer(i4b)
+    !     The last sample number/index
+    !  n_lim: integer(i4b)
+    !     The maximum index to consider when computing the correlation coefficient
+    !  Returns:
+    !  -----------------
+    !  calc_corr_coeff: double precision real number
+    !     The computed correlation coefficient of the sample set.
+    !
+
+    !default if no corr coeff is found, return a value that is non-physical.
+    !correlation coefficient is -1 <= coeff <= 1, by definition
+    calc_corr_coeff = -2.d0 
+
+    ns=min(size(spec_arr),n_samp)
+
+    if (ns > 2) then
+       allocate(xarr(ns),yarr(ns))
+       do i = 1,ns
+          xarr(i)=i*1.d0
+          yarr(i)=spec_arr(i)
+       end do
+       x_mean=sum(xarr)/ns
+       y_mean=sum(yarr)/ns
+       sig_x = sqrt(sum((xarr-x_mean)**2)/ns)
+       sig_y = sqrt(sum((yarr-y_mean)**2)/ns)
+       if (sig_x > 0.d0 .and. sig_y > 0.d0) then
+          covarr = sum((xarr-x_mean)*(yarr-y_mean))/ns
+          calc_corr_coeff = covarr/(sig_x*sig_y)
+       end if
+       deallocate(xarr,yarr)
+    end if
+
+  end function calc_corr_coeff
+
+  function calc_corr_len(spec_arr,n_samp) 
+    implicit none
+    integer(i4b),               intent(in), optional :: n_samp
+    real(dp),     dimension(:), intent(in)           :: spec_arr
+    integer(i4b)                                     :: calc_corr_len
+
+    integer(i4b) :: i, j, maxcorr, ns
+    real(dp)     :: x_mean, y_mean, sig_x, sig_y
+    real(dp), dimension(:), allocatable :: xarr, yarr,covarr
+    !
+    !  Function to calculate the sampling correlation length of a spectral parameter sampling.
+    !
+    !  Arguments:
+    !  ------------------
+    !  spec_arr: double precision array, unknown length
+    !     An array containing the sampled spectralparameters
+    !  n_samp: integer(i4b)
+    !     the total length of spec_arr of which to compute the correlation length. must be 0 < n_samp <= len(spec_arr).
+    !
+    !  Returns:
+    !  -----------------
+    !  calc_corr_len: integer(i4b)
+    !     The computed sample correlation length of the sampled spectral parameters
+    !
+
+    calc_corr_len = -1 !default if no corr length is found
+
+    maxcorr = min(n_samp/2,size(spec_arr)/2)
+    
+    allocate(xarr(maxcorr),yarr(maxcorr),covarr(maxcorr))
+
+    do i = 1,maxcorr
+       ns=maxcorr-i
+       xarr=spec_arr(1:ns)
+       yarr=spec_arr(1+i:ns+i)
+       x_mean=sum(xarr)/ns
+       y_mean=sum(yarr)/ns
+       sig_x = sqrt(sum((xarr-x_mean)**2)/ns)
+       sig_y = sqrt(sum((yarr-y_mean)**2)/ns)
+       covarr(i) = sum((xarr-x_mean)*(yarr-y_mean))/(ns*sig_x*sig_y)
+       if (covarr(i) < 0.d0) then
+          calc_corr_len = i
+          exit
+       end if
+    end do
+
+    deallocate(xarr,yarr,covarr)
+
+  end function calc_corr_len
+
   
 end module comm_utils
