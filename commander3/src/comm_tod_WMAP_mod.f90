b1a3cd956233f6a520c109521b5ec8ce6fab7f6f
Duncan <duncanjosephwatts@gmail.com>
2021-09-05 16:46:59 +0200

    Moving write statement with if statement

--- commander3/src/comm_tod_WMAP_mod.f90
+++ commander3/src/comm_tod_WMAP_mod.f90
@@ -466,7 +466,7 @@ contains
          ! Compute binned map
          allocate(d_calib(self%output_n_maps,sd%ntod, sd%ndet))
          call compute_calibrated_data(self, i, sd, d_calib)
-         if (.true. .and. i==1 .and. self%first_call) then
+         if (.false. .and. i==1 .and. self%first_call) then
             call int2string(self%scanid(i), scantext)
             if (self%verbosity > 0) write(*,*) 'Writing tod to txt'
             do k = 1, self%ndet
@@ -545,13 +545,13 @@ contains
       !epsil(1)   = 1d-8
       epsil(2:6) = 1d-6
       num_cg_iters = 0
-      if (self%myid == 0) then 
-         if (self%verbosity > 0) write(*,*) '  Running BiCG'
-      end if
 
       ! Doing this now because it's still burning in...
-      if (mod(iter-1,10*self%output_aux_maps) == 0) then
+      !if (mod(iter-1,10*self%output_aux_maps) == 0) then
         ! Solve for maps
+        if (self%myid == 0) then 
+           if (self%verbosity > 0) write(*,*) '  Running BiCG'
+        end if
         call update_status(status, "Starting bicg-stab")
         do l=1, self%output_n_maps
            if (self%verbosity > 0 .and. self%myid == 0) then
@@ -562,7 +562,7 @@ contains
                           & prefix, postfix, comp_S)
         end do
         if (self%verbosity > 0 .and. self%myid == 0) write(*,*) '  Finished BiCG'
-      end if
+      !end if
 
       call mpi_bcast(bicg_sol, size(bicg_sol),  MPI_DOUBLE_PRECISION, 0, self%info%comm, ierr)
       call mpi_bcast(num_cg_iters, 1,  MPI_INTEGER, 0, self%info%comm, ierr)
