11dd2da40749b2b8a5d6430d03a400ac800f2030
maksymbrl <maksym.brilenkov@astro.uio.no>
2021-10-15 11:44:25 +0200

    Fixed another bug per HKE example

--- commander3/src/comm_tod_mod.f90
+++ commander3/src/comm_tod_mod.f90
@@ -652,7 +652,7 @@ contains
 !!$    end if
 
     call update_status(status, "aaa")
-    if (self%L2_exist) then
+    if (trim(self%level) == 'L2' .or. self%L2_exist) then
        do i = 1, self%nscan
           call read_hdf_scan_data(self%scans(i), self, self%hdfname(i), self%scanid(i), self%ndet, &
                & detlabels, self%nhorn, self%ndiode, self%diode_names)
