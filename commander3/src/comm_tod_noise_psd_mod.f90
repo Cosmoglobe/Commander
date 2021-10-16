a8a5f18cea98a392b7e1286ed0d80989ad726df8
Duncan <duncanjosephwatts@gmail.com>
2021-10-16 13:46:25 +0200

    Fixed some merge conflicts

--- commander3/src/comm_tod_noise_psd_mod.f90
+++ commander3/src/comm_tod_noise_psd_mod.f90
@@ -48,7 +48,7 @@ module comm_tod_noise_psd_mod
      ! Class definition for basic 1/f noise PSD model
      !
      integer(i4b) :: npar                                            ! Number of free parameters
-     real(sp)     :: nu_fit(2)                                       ! Frequency range used to fit non-linear parameters
+     real(sp),     allocatable, dimension(:,:)  :: nu_fit            ! Frequency range used to fit non-linear parameters
      real(sp),     pointer :: sigma0                                 ! Pointer to xi_n(1)
      real(sp),     allocatable, dimension(:)    :: xi_n              ! Active sampling parameters, xi_n(1) = sigma0
      real(sp),     allocatable, dimension(:,:)  :: P_uni             ! Uniform prior on xi_n (n_xi,lower/upper)
@@ -114,7 +114,7 @@ contains
     real(sp),              dimension(:),   intent(in)      :: P_active_mean
     real(sp),              dimension(:),   intent(in)      :: P_active_rms
     real(sp),              dimension(:,:), intent(in)      :: P_uni
-    real(sp),              dimension(2),   intent(in)      :: nu_fit
+    real(sp),              dimension(:,:), intent(in)      :: nu_fit
     class(comm_noise_psd), pointer                         :: constructor_oof
 
     allocate(constructor_oof)
@@ -126,6 +126,7 @@ contains
 
     constructor_oof%npar = 3
     allocate(constructor_oof%xi_n(constructor_oof%npar))
+    allocate(constructor_oof%nu_fit(constructor_oof%npar, 2))
     allocate(constructor_oof%P_uni(constructor_oof%npar,2))
     allocate(constructor_oof%P_active(constructor_oof%npar,2))
     allocate(constructor_oof%P_lognorm(constructor_oof%npar))
@@ -203,7 +204,7 @@ contains
     real(sp),                   dimension(:),   intent(in)      :: P_active_mean
     real(sp),                   dimension(:),   intent(in)      :: P_active_rms
     real(sp),                   dimension(:,:), intent(in)      :: P_uni
-    real(sp),                   dimension(2),   intent(in)      :: nu_fit
+    real(sp),                   dimension(:,:), intent(in)      :: nu_fit
     class(comm_noise_psd_2oof), pointer                         :: constructor_2oof
 
     allocate(constructor_2oof)
@@ -219,6 +220,7 @@ contains
 
     constructor_2oof%npar = 5
     allocate(constructor_2oof%xi_n(constructor_2oof%npar))
+    allocate(constructor_2oof%nu_fit(constructor_2oof%npar, 2))
     allocate(constructor_2oof%P_uni(constructor_2oof%npar,2))
     allocate(constructor_2oof%P_active(constructor_2oof%npar,2))
     allocate(constructor_2oof%P_lognorm(constructor_2oof%npar))
@@ -297,7 +299,7 @@ contains
     real(sp),                        dimension(:),   intent(in)      :: P_active_mean
     real(sp),                        dimension(:),   intent(in)      :: P_active_rms
     real(sp),                        dimension(:,:), intent(in)      :: P_uni
-    real(sp),                        dimension(2),   intent(in)      :: nu_fit
+    real(sp),                        dimension(:,:), intent(in)      :: nu_fit
     class(comm_noise_psd_oof_gauss), pointer                         :: constructor_oof_gauss
 
     allocate(constructor_oof_gauss)
@@ -310,6 +312,7 @@ contains
 
     constructor_oof_gauss%npar = 6
     allocate(constructor_oof_gauss%xi_n(constructor_oof_gauss%npar))
+    allocate(constructor_oof_gauss%nu_fit(constructor_oof_gauss%npar, 2))
     allocate(constructor_oof_gauss%P_uni(constructor_oof_gauss%npar,2))
     allocate(constructor_oof_gauss%P_active(constructor_oof_gauss%npar,2))
     allocate(constructor_oof_gauss%P_lognorm(constructor_oof_gauss%npar))
@@ -377,5 +380,102 @@ contains
     eval_noise_psd_oof_gauss_corr = S1 + S2
 
   end function eval_noise_psd_oof_gauss_corr
+
+  function constructor_oof_f(P_active_mean, P_active_rms, P_uni, nu_fit)
+    ! 
+    ! Constructor for two-component 1/f noise PSD object, where
+    !     
+    !     P(nu) = sigma0^2 * (1 + (nu/fknee)^alpha + g*(nu/fknee))
+    ! 
+    ! Arguments
+    ! --------- 
+    ! xi_n_def: sp (array)
+    !          5-element array containing default {sigma0, fknee, alpha, fknee2, alpha2}, where
+    !          [sigma0] = du/volts/tod unit, [alpha] = 1, and [fknee] = Hz
+    ! P_uni: sp (2D array)
+    !          Array containing absolute upper and lower limits for each parameter (npar,upper/lower)
+    ! P_active: sp (2D array)
+    !          Array containing informative priors for each parameter (npar,mean/rms)
+    ! nu_fit: sp (2-element array)
+    !          Array with [nu_min,nu_max] in Hz, defining ranged used for fittig non-linear parameters
+    ! 
+    implicit none
+    real(sp),                        dimension(:),   intent(in)      :: P_active_mean
+    real(sp),                        dimension(:),   intent(in)      :: P_active_rms
+    real(sp),                        dimension(:,:), intent(in)      :: P_uni
+    real(sp),                        dimension(:,:), intent(in)      :: nu_fit
+    class(comm_noise_psd_oof_f),     pointer                         :: constructor_oof_f
+
+    allocate(constructor_oof_f)
+
+    if (P_active_mean(FKNEE) <= 0.0)       write(*,*) 'comm_noise_psd error: fknee prior mean less than zero'
+    if (P_uni(FKNEE,1) <= 0.0)             write(*,*) 'comm_noise_psd error: Lower fknee prior less than zero'
+    if (P_uni(FKNEE,1) > P_uni(FKNEE,2))   write(*,*) 'comm_noise_psd error: Lower fknee prior higher than upper prior'
+    if (P_uni(ALPHA,1) > P_uni(ALPHA,2))   write(*,*) 'comm_noise_psd error: Lower alpha prior higher than upper prior'
+
+    constructor_oof_f%npar = 4
+    allocate(constructor_oof_f%xi_n(constructor_oof_f%npar))
+    allocate(constructor_oof_f%nu_fit(constructor_oof_f%npar, 2))
+    allocate(constructor_oof_f%P_uni(constructor_oof_f%npar,2))
+    allocate(constructor_oof_f%P_active(constructor_oof_f%npar,2))
+    allocate(constructor_oof_f%P_lognorm(constructor_oof_f%npar))
+
+    constructor_oof_f%xi_n          = P_active_mean
+    !write(*,*) size(constructor_oof_f%P_uni, 1), size(constructor_oof_f%P_uni, 2), size(P_uni, 1), size(P_uni,2)
+    !write(*,*) P_uni
+    constructor_oof_f%P_uni         = P_uni
+    constructor_oof_f%P_active(:,1) = P_active_mean
+    constructor_oof_f%P_active(:,2) = P_active_rms
+    constructor_oof_f%nu_fit        = nu_fit
+    constructor_oof_f%P_lognorm     = [.false., .true., .false., .false.] !  [sigma0, fknee, alpha, gamma]
+
+    constructor_oof_f%sigma0 => constructor_oof_f%xi_n(1)
+
+  end function constructor_oof_f
+  
+  function eval_noise_psd_oof_f_full(self, nu)
+    ! 
+    ! Evaluation routine for 2-component 1/f  + gamma*f noise PSD object
+    ! 
+    ! Arguments
+    ! ---------
+    ! self:    derived type (comm_noise_psd)
+    !          Basic noise PSD object
+    ! nu:      sp (scalar)
+    !          Frequency (in Hz) at which to evaluate PSD
+    ! 
+    implicit none
+    class(comm_noise_psd_oof_f),         intent(in)      :: self
+    real(sp),                            intent(in)      :: nu
+    real(sp)                                             :: eval_noise_psd_oof_f_full
+ 
+    eval_noise_psd_oof_f_full = self%xi_n(SIGMA0)**2 + self%eval_corr(nu)
+
+  end function eval_noise_psd_oof_f_full
+
+  function eval_noise_psd_oof_f_corr(self, nu)
+    ! 
+    ! Evaluation routine for 1/f noise + gamma*f PSD object; correlated noise only
+    ! 
+    ! Arguments
+    ! ---------
+    ! self:    derived type (comm_noise_psd)
+    !          Basic noise PSD object
+    ! nu:      sp (scalar)
+    !          Frequency (in Hz) at which to evaluate PSD
+    ! 
+    implicit none
+    class(comm_noise_psd_oof_f),          intent(in)      :: self
+    real(sp),                            intent(in)      :: nu
+    real(sp)                                             :: eval_noise_psd_oof_f_corr
+
+    real(sp) :: S1, S2
+
+    S1 = self%xi_n(SIGMA0)**2 * (nu/self%xi_n(FKNEE))**self%xi_n(ALPHA)
+    S2 = self%xi_n(SIGMA0)**2 * self%xi_n(G_AMP) * (nu/self%xi_n(FKNEE))
+
+    eval_noise_psd_oof_f_corr = S1 + S2
+
+  end function eval_noise_psd_oof_f_corr
   
 end module comm_tod_noise_psd_mod
