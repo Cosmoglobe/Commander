e5cd50666d5c0a48d4ca69c8b658926b5cec1162
Hans Kristian Eriksen <h.k.k.eriksen@astro.uio.no>
2021-11-26 00:16:53 +0100

    Fixes Cl TT prior and HDF size problem

--- commander3/src/comm_map_mod.f90
+++ commander3/src/comm_map_mod.f90
@@ -628,23 +628,28 @@ contains
     type(hdf_file),   intent(in)    :: hdffile
     character(len=*), intent(in)    :: hdfpath
 
-    integer(i4b) :: i, nmaps, npix, np, ierr
+    integer(i4b) :: i, nmaps, npix, np, ierr, ext(2)
     real(dp),     allocatable, dimension(:,:) :: map, buffer
     integer(i4b), allocatable, dimension(:)   :: p
     integer(i4b), dimension(MPI_STATUS_SIZE)  :: mpistat
     
     ! Only the root actually writes to disk; data are distributed via MPI
     if (self%info%myid == 0) then
+       call get_size_hdf(hdffile, trim(adjustl(hdfpath)), ext)
+       if (self%info%npix /= ext(1) .or. self%info%nmaps > ext(2)) then
+          write(*,*) 'Error: Inconsistent field size in HDF file ', trim(adjustl(hdfpath))
+          stop
+       end if
        npix  = self%info%npix
-       nmaps = self%info%nmaps
-       allocate(p(npix), map(0:npix-1,nmaps))
+       allocate(p(npix), map(0:npix-1,ext(2)))
        call read_hdf_dp_2d_buffer(hdffile, trim(adjustl(hdfpath)), map)
-       self%map = map(self%info%pix,:)
+       nmaps = min(self%info%nmaps,ext(2))
+       self%map(:,1:nmaps) = map(self%info%pix,1:nmaps)
        do i = 1, self%info%nprocs-1
           call mpi_recv(np,       1, MPI_INTEGER, i, 98, self%info%comm, mpistat, ierr)
           call mpi_recv(p(1:np), np, MPI_INTEGER, i, 98, self%info%comm, mpistat, ierr)
           allocate(buffer(np,nmaps))
-          buffer = map(p(1:np),:) 
+          buffer(:,1:nmaps) = map(p(1:np),1:nmaps) 
           call mpi_send(buffer,      size(buffer), MPI_DOUBLE_PRECISION, i, 98, &
             & self%info%comm, ierr)
           deallocate(buffer)
@@ -866,7 +871,7 @@ contains
     character(len=*),       intent(in)    :: hdfpath
     logical(lgt),           intent(in)    :: read_map
 
-    integer(i4b) :: i, l, m, j, lmax, nmaps, ierr, nalm, npix
+    integer(i4b) :: i, l, m, j, lmax, nmaps, ierr, nalm, npix, ext(2)
     real(dp),     allocatable, dimension(:,:) :: alms, map
     !integer(i4b), allocatable, dimension(:)   :: p
     !integer(i4b), dimension(MPI_STATUS_SIZE)  :: mpistat
@@ -879,18 +884,20 @@ contains
     if (.not. read_map) then
        if (lmax < 0) return
       ! Only the root actually reads from disk; data are distributed via MPI
-      allocate(alms(0:nalm-1,nmaps))
+       call get_size_hdf(hdffile, trim(adjustl(hdfpath)), ext)
+      allocate(alms(0:nalm-1,ext(2)))
       if (self%info%myid == 0) call read_hdf_dp_2d_buffer(hdffile, trim(adjustl(hdfpath)), alms)
       call mpi_bcast(alms, size(alms),  MPI_DOUBLE_PRECISION, 0, self%info%comm, ierr)
       do i = 0, self%info%nalm-1
         call self%info%i2lm(i, l, m)
         j = l**2 + l + m
-        self%alm(i,:) = alms(j,:)
+        self%alm(i,1:nmaps) = alms(j,1:nmaps)
       end do
       deallocate(alms)
     else
       ! Only the root actually reads from disk; data are distributed via MPI
-      allocate(map(0:npix-1,nmaps))
+       call get_size_hdf(hdffile, trim(adjustl(hdfpath)), ext)
+      allocate(map(0:npix-1,ext(2)))
       if (self%info%myid == 0) call read_hdf_dp_2d_buffer(hdffile, trim(adjustl(hdfpath)), map)
       call mpi_bcast(map, size(map),  MPI_DOUBLE_PRECISION, 0, self%info%comm, ierr)
 !!$      if (self%info%myid == 0) then
