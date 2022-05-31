89e83e6e019b7cf23dc0a052059b658729dda00a
Metin San <metinisan@gmail.com>
2022-05-31 16:37:20 +0200

    Added dirbe_mod to makefiles. Update data_mod to accept dirbe tod types. Updated default parameters

--- cmake/projects/commander3.cmake
+++ cmake/projects/commander3.cmake
@@ -68,6 +68,7 @@ set(sources
 	${COMMANDER3_SOURCE_DIR}/comm_tod_orbdipole_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_pointing_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_wmap_mod.f90
+	${COMMANDER3_SOURCE_DIR}/comm_tod_dirbe_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_quiet_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_quiet_smod.f90
   ${COMMANDER3_SOURCE_DIR}/comm_tod_spider_mod.f90
