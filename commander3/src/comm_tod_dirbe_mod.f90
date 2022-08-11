998f064a923cdd883b1a174f8a4a40259b7f22b8
Duncan <duncanjosephwatts@gmail.com>
2022-08-11 12:58:25 +0200

    Commented out DIRBE offending lines

--- commander3/src/comm_tod_dirbe_mod.f90
+++ commander3/src/comm_tod_dirbe_mod.f90
@@ -108,14 +108,14 @@ contains
     allocate(constructor%xi_n_P_rms(constructor%n_xi))
     
     constructor%xi_n_P_rms      = [-1.0, 0.1, 0.2] ! [sigma0, fknee, alpha]; sigma0 is not used
-    if (.true.) then
-       constructor%xi_n_nu_fit     = [0.,    0.200] ! More than max(2*fknee_default)
-       constructor%xi_n_P_uni(2,:) = [0.001, 0.45]  ! fknee
-       constructor%xi_n_P_uni(3,:) = [-2.5, -0.4]   ! alpha
-    else
-       write(*,*) 'Invalid DIRBE frequency label = ', trim(constructor%freq)
-       stop
-    end if
+    !if (.true.) then
+    !   constructor%xi_n_nu_fit     = [0.,    0.200] ! More than max(2*fknee_default)
+    !   constructor%xi_n_P_uni(2,:) = [0.001, 0.45]  ! fknee
+    !   constructor%xi_n_P_uni(3,:) = [-2.5, -0.4]   ! alpha
+    !else
+    !   write(*,*) 'Invalid DIRBE frequency label = ', trim(constructor%freq)
+    !   stop
+    !end if
 
     ! Initialize common parameters
     call constructor%tod_constructor(cpar, id_abs, info, tod_type)
@@ -161,18 +161,18 @@ contains
     ! Load the instrument file
     call constructor%load_instrument_file(nside_beam, nmaps_beam, pol_beam, cpar%comm_chain)
 
-    do i=1, constructor%ndet
-      call init_noise_model(constructor, i)
-    end do
+    !do i=1, constructor%ndet
+    !  call init_noise_model(constructor, i)
+    !end do
 
     ! Allocate sidelobe convolution data structures
     allocate(constructor%slconv(constructor%ndet), constructor%orb_dp)
     constructor%orb_dp => comm_orbdipole(constructor%mbeam)
 
     ! Initialize all baseline corrections to zero
-    do i = 1, constructor%nscan
-       constructor%scans(i)%d%baseline = 0.d0
-    end do
+    !do i = 1, constructor%nscan
+    !   constructor%scans(i)%d%baseline = 0.d0
+    !end do
 
   end function constructor
 
