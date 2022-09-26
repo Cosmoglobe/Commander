8a43681fbf1baf248b3f8785f640d9a7420f8b9e
Duncan <duncanjosephwatts@gmail.com>
2022-09-26 21:33:19 +0200

    Added warning for when smoothed rms is not loaded

--- commander3/src/comm_data_mod.f90
+++ commander3/src/comm_data_mod.f90
@@ -165,7 +165,7 @@ contains
             ! Class initialisation 
             data(n)%tod => comm_QUIET_tod(cpar, i, data(n)%info, data(n)%tod_type)
           else if (trim(cpar%ds_tod_type(i)) == 'none') then
-             !write(*,*) 'Warning: TOD analysis enabled for TOD type "none"'
+            if (cpar%myid == 0) write(*,*) '|  Warning: TOD analysis enabled for TOD type "none"'
           else
              write(*,*) 'Unrecognized TOD experiment type = ', trim(data(n)%tod_type)
              stop
@@ -323,6 +323,7 @@ contains
           else if (trim(cpar%ds_noise_rms_smooth(i,j)) /= 'none') then
              data(n)%N_smooth(j)%p => comm_N_rms(cpar, data(n)%info, n, i, j, data(n)%mask, handle)
           else
+             if (cpar%myid == 0) write(*,*) '|  Warning: smoothed rms map not being loaded'
              nullify(data(n)%N_smooth(j)%p)
           end if
        end do
