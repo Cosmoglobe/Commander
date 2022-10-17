f2a5fd7eb150b3d66667115fec2e82b4eafab80d
Metin San <metinisan@gmail.com>
2022-10-17 11:09:51 +0200

    Fix bugs with `install_ita` on hyades

--- install_ita.sh
+++ install_ita.sh
@@ -106,12 +106,12 @@ then
     build_dir="build_bee47_$toolchain"
   elif [[ "${HOSTNAME}" =~ $hya5 ]]; then
     build_dir="build_hya5_$toolchain"
-  elif [[ "${HOSTNAME}" =~ $hya5 ]]; then
+  elif [[ "${HOSTNAME}" =~ $hya6 ]]; then
     build_dir="build_hya6_$toolchain"
   elif [[ "${HOSTNAME}" =~ $hya79 ]]; then
-    build_dir="build_hya716_$toolchain"
+    build_dir="build_hya79_$toolchain"
   elif [[ "${HOSTNAME}" =~ $hya1016 ]]; then
-    build_dir="build_hya716_$toolchain"
+    build_dir="build_hya1016_$toolchain"
   elif [[ "${HOSTNAME}" =~ $hya13 ]]; then
     build_dir="build_hya13_$toolchain"
 	fi
