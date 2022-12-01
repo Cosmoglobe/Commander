975dfcd8bcc8d0106d99e0b9e51e769743a93e44
Duncan <duncanjosephwatts@gmail.com>
2022-12-01 12:58:33 +0100

    Turned off data selection, ncorr and res on first iter

--- commander3/src/comm_tod_wmap_mod.f90
+++ commander3/src/comm_tod_wmap_mod.f90
@@ -425,7 +425,7 @@ contains
       bp_corr               = .true.                 ! by default, take into account differences in bandpasses. (WMAP does not do this in default analysis)
       bp_corr               = (bp_corr .or. sample_rel_bandpass) ! Bandpass is necessary to include if bandpass sampling is happening.
       select_data           = .false.         ! no data selection
-      select_data           = self%first_call ! only perform data selection the first time
+      !select_data           = self%first_call ! only perform data selection the first time
       output_scanlist       = mod(iter-1,10) == 0    ! only output scanlist every 10th iteration
 
 
@@ -442,7 +442,7 @@ contains
       if (self%output_aux_maps > 0) then
          if (mod(iter-1,self%output_aux_maps) == 0)    self%output_n_maps = 3
          if (mod(iter-1,10*self%output_aux_maps) == 0) self%output_n_maps = 6
-         if (iter .eq. 1)                              self%output_n_maps = 1
+         !if (iter .eq. 1)                              self%output_n_maps = 1
       end if
 
       !if (mod(iter-1, 10) == 0) call self%precompute_M_lowres
