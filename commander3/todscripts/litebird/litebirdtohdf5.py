aaf627f583b6cae561dc8a75e5c3d99a2acadd6a
Eirik Gjerløw <eirik.gjerlow@astro.uio.no>
2022-12-09 04:08:43 +0100

    Update litebirdtohdf

--- commander3/todscripts/litebird/litebirdtohdf5.py
+++ commander3/todscripts/litebird/litebirdtohdf5.py
@@ -11,9 +11,10 @@ import h5py
 
 def main():
     parser = argparse.ArgumentParser()
-    outpath = '/home/eirik/data/litebird_sims/'
+    outpath = '/mn/stornext/u3/eirikgje/data/litebird_tods/'
     name = 'litebird'
-    version = 'v1.3'
+    version = '1.0'
+    imo_version = 'v1.3'
     dicts = None
     overwrite = True
 
@@ -22,10 +23,22 @@ def main():
     manager_dicts = {}
 
     comm_tod = tod.commander_tod(outpath, name, version, manager_dicts, overwrite)
-    make_od(comm_tod, 'L1-060', 1, None, version)
+    make_ods(comm_tod, 'L1-060', None, imo_version)
+
+
+def create_new_tod(comm_tod, od, freq, fsamps, nside, dets, polang):
+    comm_tod.init_file(int(freq[-3:]), od, mode='w')
+    prefix ='/common'
+    comm_tod.add_field(prefix + '/fsamp', fsamps)
+    comm_tod.add_field(prefix + '/nside', [nside])
+    comm_tod.add_field(prefix + '/det', np.string_(', '.join(dets)))
+    comm_tod.add_field(prefix + '/polang', polang)
+    comm_tod.add_field(prefix + '/mbang', [0.0]*len(dets))
+    return comm_tod
 
     
-def make_od(comm_tod, freq, od, args, imo_version):
+def make_ods(comm_tod, freq, args, imo_version,
+             simpath='/mn/stornext/d16/cmbco/bp/mathew/litebird/sim0000/LFT_L1-060/tods/'):
     if freq[0] == 'L':
         instrument = 'LFT'
     elif freq[0] == 'M':
@@ -34,7 +47,6 @@ def make_od(comm_tod, freq, od, args, imo_version):
         instrument = 'HFT'
     else:
         raise ValueError('Frequency format not recognized - must start with L, M or H')
-    comm_tod.init_file(int(freq[-3:]), od, mode='w')
     imo_db_interface = lbs.Imo()
     imo = LitebirdImo(imo_db_interface, imo_version, instrument) 
     dets = imo.get_channel_dets(freq)
@@ -43,8 +55,8 @@ def make_od(comm_tod, freq, od, args, imo_version):
     for fsamp in fsamps:
         assert(fsamp == fsamps[0])
 
-#    chunk_size = 2 ** 22
-    chunk_size = 2 ** 16
+    chunk_size = 2 ** 22
+#    chunk_size = 2 ** 16
     scan_time = chunk_size / fsamps[0] # in seconds. About 2.6 days when the sampling frequency is 19 Hz
 
     if instrument in ('LFT', 'HFT'):
@@ -52,31 +64,28 @@ def make_od(comm_tod, freq, od, args, imo_version):
     else:
         polang = [int(it.split('_')[3][:1]) + 90 * int(it.split('_')[3][2] == 'B') for it in dets]
 
-    prefix ='/common'
-    comm_tod.add_field(prefix + '/fsamp', fsamps)
-    comm_tod.add_field(prefix + '/nside', [nside])
-    comm_tod.add_field(prefix + '/det', np.string_(', '.join(dets)))
-    comm_tod.add_field(prefix + '/polang', polang)
-    comm_tod.add_field(prefix + '/mbang', [0.0]*len(dets))
-    for field in ('tod', 'pointings', 'psi'):
-        print(f"Field: {field}")
-        with LitebirdTodReader('/home/eirik/data/litebird_sims/',
-                               'LFT_L1-060_obs_cmb',
-                               start_day=100,
-                               end_day=105,
-                               field=field) as reader:
-            for curr_scan_id, chunk in enumerate(reader.iterate_chunks(chunk_size)):
-                print(f"Curr scan id: {curr_scan_id}")
+    with LitebirdTodReader(simpath,
+                           'LFT_L1-060_obs_cmb',
+                           start_day=100,
+                           end_day=105, 
+                           fields=['tod', 'pointings', 'psi']) as reader:
+        for curr_scan_id, chunk in enumerate(reader.iterate_chunks(chunk_size)):
+            print(f"Curr scan id: {curr_scan_id}")
+            comm_tod = create_new_tod(comm_tod, curr_scan_id+1, freq, fsamps, nside, dets, polang)
+            for field_idx, field in enumerate(('tod', 'pointings', 'psi')):
                 for i, detector in enumerate(dets):
 #                    print(f"Detector: {detector}")
                     prefix = f'{curr_scan_id:06}/{detector}' 
                     if field == 'pointings':
-                        comm_tod.add_field(f'{prefix}/theta', chunk[i, :, 0])
-                        comm_tod.add_field(f'{prefix}/phi', chunk[i, :, 1])
+                        comm_tod.add_field(f'{prefix}/theta', chunk[field_idx][i, :, 0])
+                        comm_tod.add_field(f'{prefix}/phi', chunk[field_idx][i, :, 1])
                     elif field == 'tod':
-                        comm_tod.add_field(f'{prefix}/tod', chunk[i, :])
+                        comm_tod.add_field(f'{prefix}/tod', chunk[field_idx][i, :])
                     elif field == 'psi':
-                        comm_tod.add_field(f'{prefix}/psi', chunk[i, :])
+                        comm_tod.add_field(f'{prefix}/psi', chunk[field_idx][i, :])
+            comm_tod.finalize_file()
+    comm_tod.make_filelists()
+
 
 if __name__ == '__main__':
     main()
