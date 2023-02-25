94a5da8c49db80967a3350bb041cf33fb4e34d3e
maksymbrl <maksym.brilenkov@astro.uio.no>
2023-02-25 09:35:56 +0100

    Progress backup

--- commander3/todscripts/litebird/litebirdtohdf5_v2.py
+++ commander3/todscripts/litebird/litebirdtohdf5_v2.py
@@ -16,6 +16,9 @@ import numpy as np
 import healpy as hp
 import litebird_sim as lbs
 import joblib
+import matplotlib.pyplot as plt
+# to see progress bar
+from tqdm import tqdm
 #---------------------------------------------
 # Local Development Libs
 #---------------------------------------------
@@ -42,23 +45,260 @@ suitable format
 """
 
 
+def get_data(nside, k, dfile):
+
+    print(f"Test statement {k}")
+    with h5py.File(dfile, 'r') as readin_file:
+        tod_cmb  = np.array(readin_file.get("tod_cmb"))
+        tod_dip  = np.array(readin_file.get("tod_dip")) 
+        tod_fg   = np.array(readin_file.get("tod_fg")) 
+        tod_wn   = np.array(readin_file.get("tod_wn")) 
+        tod_cadd = tod_cmb + tod_dip + tod_fg + tod_wn
+        # TODO: Add calculation of pixels <= pointings
+        pointings = np.array(readin_file.get("pointings"))
+        theta     = pointings[:,:,0]
+        phi       = pointings[:,:,1]
+        pixels    = hp.ang2pix(nside, theta, phi)
+
+
+    # Keeping track of current index
+    #return {k: tod_cadd}
+    return (tod_cadd, pixels)
+
+
+
+
 def main():
     """
     Main method of the script 
     """
-    lbdata_dir = pathlib.Path("/Users/maksymb/Desktop/litebird_db/data") 
-    output_dir = pathlib.Path(__file__).parent.joinpath("test_litebird_sims")
+    nprocs = 64 #joblib.cpu_count() - 28 #16
+    #lbdata_dir = pathlib.Path("/Users/maksymb/Desktop/litebird_db/data") 
+    lbdata_dir = pathlib.Path(
+            "/mn/stornext/d22/cmbco/litebird/e2e_ns512/sim0000/detectors_LFT_L1-060_T+B/tods"
+            )
+    #output_dir = pathlib.Path(__file__).parent.joinpath("test_litebird_sims")
+    output_dir = pathlib.Path(
+            "/mn/stornext/d5/data/maksymb/litebird"
+            )
     if not pathlib.Path.is_dir(output_dir):
         pathlib.Path.mkdir(output_dir)
 
     # Getting file names inside specified directory and removing the path component
-    lbdata_files = sorted(lbdata_dir.rglob('*.hdf5')) 
-    lbdata_files = [data_file.name for data_file in lbdata_files]
+    lbdata_files = sorted(lbdata_dir.rglob('*_rank????.hdf5')) 
+    #lbdata_files = [data_file.name for data_file in lbdata_files]
 
-    print(f"{lbdata_files}")
+    # Getting all the data sizes so later will split everything into chunks
+    with h5py.File(lbdata_files[0], 'r') as readin_file:
+        tod_cmb  = np.array(readin_file.get("tod_cmb"))
+
+    todlen = len(tod_cmb[0])
+
+    nfiles = len(lbdata_files)
+
+    # These parameters are specified by the user (!)
+    # The size of one scan in a new file (to be created/output) 
+    scan_size = 2**16
+    # The amount of scans to have in a given (to be created/output) file
+    scan_num  = 20 
+    # The amount of time (in sec) the given scan will have 
+    # (this value is unused and it just for us to see)
+    scan_time = scan_size / 19.0
+
+    # Number of scans of length scan_size (out of the entire length todlen) to
+    # obtain from a given simulation file 
+    N_scans_from_file = todlen / scan_size 
+    # If this number is less then scan_num, then we open additional file
+    # and to do that we need to know a number of files to open 
+    #if N_scans_from_file < scan_num:
+    # Minimum number of files to open to get a single output file (with a
+    # specified number of scans and length of a single scan). Getting int
+    # number and adding one to ensure that we do not go less then 1 file. 
+    # [**NOTE**]: Each processor will get this value to work with in parallel. 
+    nfiles_to_open = scan_num // N_scans_from_file + 1
+    #elif N_scans_from_file >= scan_num:
+    #    nfiles_to_open = 1
+        # 
+    print("#------------------")
+    print(f"Total number of simulated files to work with: {nfiles}")
+    print(f"The length of a single TOD: {todlen}")
+    print(f"The scan size in a new file: {scan_size} ({scan_time:.2f} s or {scan_time/3600:.2f} hrs)")
+    print(f"Total Number of scans in a new file: {scan_num}")
+    print(f"Number of scans to obtain from simulated input file: {N_scans_from_file:.2f}")
+    print(f"The number ratio of new scan to old scan: {scan_num // N_scans_from_file:.2f}")
+    print(f"Number of files to open (per CPU process): {nfiles_to_open:.2f}")
+    print("#------------------")
 
 
-    print(2**22)
+    nfiles_to_open = int(nfiles_to_open)
+    # Splitting it the workload (number of files) into equal batches
+    batches = [ int(nfiles_to_open * nprocs)
+            for i in range( nfiles // int(nfiles_to_open * nprocs) ) ]
+    # Appending the remnant batch to the list of batches
+    batches.append(nfiles - np.sum(batches))
+    # 
+    file_ranges = [0]
+    for i in range(len(batches)):
+        if i == len(batches)-1:
+            file_ranges.append(nfiles)
+        else:
+            file_ranges.append((i+1)*batches[i])
+
+    print(f"Batches are: {batches}")
+    print(f"File ranges to process: {file_ranges}")
+    print("#------------------")
+    exit()
+
+    #exit()
+    # Adding unequal part
+    #for i in range( nfiles % (nfiles_to_open * nprocs) ):
+    #    workloads[i] += 1
+    #print(workloads)
+    #print(len(workloads))
+    #print(np.sum(workloads))
+
+    #print(88768128 / 192)
+    #print(f"{lbdata_files}")
+
+    # TODO: do the loop in batches (just split the total data files into
+    # batches nfiles_tot/4) and do the processing in batches
+    #remnants = np.zeros((48, something here))
+    nside = 512
+    #print(len(lbdata_files[:batches[0]]))
+    for i in range(1, 2):#len(file_ranges)):
+    #for i in tqdm(range(1, 2)):#len(batches)):
+        print(f"Working with i = {i}:  {file_ranges[i-1]} -- {file_ranges[i]}")
+
+        #with joblib.parallel_backend(backend="multiprocessing", n_jobs=nprocs):
+        # This will pass e.g. 192 files (1 batch) if scan_size = 2**16,
+        # scan_num = 20, nprocs = 64. In such a way each core will get 3 files
+        # to work with. The resulting array will be of size (192, 48, 462334) 
+        superTOD = joblib.Parallel(n_jobs=nprocs,
+                verbose=2)(joblib.delayed(get_data)
+                (nside, k, dfile) 
+                for k, dfile in enumerate(lbdata_files[file_ranges[i-1]:file_ranges[i]])) 
+                #(nside, k, dfile) for k, dfile in enumerate(lbdata_files))
+
+        superTOD = list(map(list, zip(*superTOD)))
+        superPix = superTOD[1]
+        superTOD = superTOD[0]
+        print(np.concatenate(superTOD, axis=1).shape)
+        superTOD = np.concatenate(superTOD, axis=1)
+        superPix = np.concatenate(superPix, axis=1)
+        # TODO: Split these into chunks of equal length using the end number of scans
+        print(f"TOD[0]:\n{superTOD[0]}")
+        print(superTOD.shape)
+        print(f"Pix[0]:\n{superPix[0]}")
+        print(f"Pix[4]:\n{superPix[4]}")
+        print(superPix.shape)
+
+        # TODO: 
+        # Save the superTOD chunk into a file 
+
+        #make_od()
+
+
+    exit()
+
+    #toddir = "/Users/maksymb/Desktop/litebird_db/maksymb_lb/commander3/todscripts/litebird/test_litebird_sims"
+    #todfile = "LB_L1-060_000001.h5"
+    #outtod = h5py.File(f"{toddir}/{todfile}")
+    nside = 512
+    outmap = np.zeros((48, 12 * nside ** 2))
+    nhits = np.zeros((48, 12 * nside ** 2))
+
+    #scans = [scan for scan in list(outtod) if scan != 'common']
+
+    #print(list(outtod[scans[0]]))
+
+    #det_names = [det for det in list(outtod[scans[0]]) if det != 'common'] 
+
+    #print(scans)
+    #for scan in scans:
+    #    print(scan)
+    for i in range(48):
+        #currscan_pixs = outtod[scan][detector]['pix']
+        #print(currscan_pixs)
+        #currscan_tod  = outtod[scan][detector]['tod']
+        #print(currscan_tod)
+        #nhits[i, currscan_pixs] += 1
+        #outmap[i, currscan_pixs] += currscan_tod
+        nhits[i, superPix[i]] += 1
+        outmap[i, superPix[i]] += superTOD[i]
+    print("Test message")
+    outmap[np.where(nhits != 0)] /= nhits[np.where(nhits != 0)]
+    outmap[np.where(nhits == 0)] = hp.UNSEEN
+
+    for i in range(48):
+        print(f"Working with sim_{i:03}.png")
+        hp.mollview(outmap[i])
+        plt.savefig(
+                #f"/Users/maksymb/Desktop/litebird_db/maksymb_lb/commander3/todscripts/litebird/test_litebird_sims/sim_{i:03}.png", dpi=800
+                f"/mn/stornext/u3/maksymb/commander/maksymb_lb/commander3/todscripts/litebird/test_litebird_sims/sim_{i:03}.png", dpi=800
+                )
+                #'/home/eirik/temp/sim_{i:03}.png', dpi=800)
+        plt.clf()
+    exit()
+
+
+
+
+    #print(superTOD[3])
+    #print(superTOD[5])
+    #(joblib.delayed(concat_data)(superTOD[:][k]) for k in range(48))
+    #print(np.shape(superTOD))
+    #print(np.transpose(superTOD, axes=(48,len(superTOD[0][0])*10)).shape)
+    # Convert list of dictionaries into a dictionary 
+    # (in this way we ensure that each data array has its own index)
+    #superTOD = {k: v for d in superTOD for k, v in d.items()}
+    # Populating the new array with correct indeces (in order)
+    #superTOD = [v for k, v in superTOD.items()]
+    #for i in range(len(superTOD)):
+    #    superTOD2 = np.concatenate()
+    #superTOD = np.concatenate( ) for i in range(len(superTOD))
+    #print(superTOD.shape)
+
+
+
+    #print(f"Number of files {len(lbdata_files)}")
+    ##tod_tot = np.array(h5py.File(lbdata_dir/lbdata_files[0],"r").get)
+    for i, dfile in enumerate(lbdata_files):
+        with h5py.File(lbdata_dir / dfile, 'r') as readin_file:
+            tod_cmb  = np.array(readin_file.get("tod_cmb"))
+            tod_dip  = np.array(readin_file.get("tod_dip")) 
+            tod_fg   = np.array(readin_file.get("tod_fg")) 
+            tod_wn   = np.array(readin_file.get("tod_wn")) 
+            tod_cadd = tod_cmb + tod_dip + tod_fg + tod_wn
+
+            pointings = np.array(readin_file.get("pointings"))
+            theta     = pointings[:,:,0]
+            phi       = pointings[:,:,1]
+            pixels    = hp.ang2pix(nside, theta, phi)
+            if i == 0:
+                tod_tot = tod_cadd 
+                pix_tot = pixels
+            else: 
+                tod_tot = np.concatenate((tod_tot, tod_cadd),axis=1)
+                pix_tot = np.concatenate((pix_tot, pixels),axis=1)
+            #print(tod_tot.shape)
+            #print(tod_cadd.shape)
+        #print(dfile)
+    print("Test")
+    print(tod_tot[0])
+    print(tod_tot.shape)
+    print(pix_tot[0])
+    print(pix_tot[4])
+    print(pix_tot.shape)
+    #print(tod_tot[5])
+    #arrs = np.array_split(tod_tot, 2**15)
+    #for i in range(3):
+    #    print(arrs.shape)
+
+
+    exit()
+
+
+
     version = np.string_('0.0.1')
     # To access the database etc.
     freqs = ['L1-060']
