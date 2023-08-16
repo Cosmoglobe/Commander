dcb6947042721ad4a58810503fdad7aa812ab1c5
Artem Basyrov <artemba@owl30.uio.no>
2023-08-16 16:34:35 +0200

    added comm_tod_cr_mod for cosmic rays

--- cmake/projects/commander3.cmake
+++ cmake/projects/commander3.cmake
@@ -63,6 +63,7 @@ set(sources
 	${COMMANDER3_SOURCE_DIR}/comm_tod_lfi_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_lfi_smod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_hfi_mod.f90
+        ${COMMANDER3_SOURCE_DIR}/comm_tod_cr_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_gain_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_gain_smod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_noise_mod.f90
