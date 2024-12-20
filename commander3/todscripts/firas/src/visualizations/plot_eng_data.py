5a3b1f843318ec357d6b5cc8d4ac6f437ec0af8b
Duncan <duncanjosephwatts@gmail.com>
2024-12-20 18:53:46 +0100

    Fixed typo in creating h5 files

--- commander3/todscripts/firas/src/visualizations/plot_eng_data.py
+++ commander3/todscripts/firas/src/visualizations/plot_eng_data.py
@@ -53,7 +53,6 @@ grts = ['collimator', 'dihedral', 'ical', 'mirror', 'refhorn', 'skyhorn',
 data = h5py.File('/mn/stornext/d16/cmbco/ola/firas/initial_data/fdq_eng_new.h5')
 
 xcal_pos = data['en_xcal/pos'][()].mean(axis=1)
-print(xcal_pos)
 
 bol_cmd_bias = data['en_stat/bol_cmd_bias'][()]
 cmd_inds = np.where(np.any(np.gradient(bol_cmd_bias, axis=0) != 0, axis=1))[0]
@@ -97,21 +96,10 @@ labels2 = ['dwell_stat', 'grt_addr', 'hot_spot_cmd', 'lvdt_stat',
 
 for lab in labels2:
     word = data[f'en_stat/{lab}'][()]
-    print(lab)
-    for i in range(len(word[0])):
-        print(np.unique(word[:,i]))
     if len(word.shape) == 0:
         cmds = np.hstack((cmds, word[:,np.newaxis]))
     else:
         cmds = np.hstack((cmds, word))
-
-(589069, 2)
-(589069, 2)
-(589069, 2)
-(589069, 2)
-(589069, 4)
-(589069, 2)
-(589069, 2)
 
 labels2 = ['dwell_stat', 'dwell_stat2', 'grt_addr', 'grt_addr2', 'hot_spot_cmd',
         'hot_spot_cmd2', 'lvdt_stat', 'lvdt_stat2',
@@ -119,9 +107,12 @@ labels2 = ['dwell_stat', 'dwell_stat2', 'grt_addr', 'grt_addr2', 'hot_spot_cmd',
         'micro_stat_bus4', 'power_a_status', 'power_a_status2',
         'power_b_status', 'power_b_status2']
 #labs = ['bol cmd bias'] + [f'word_{i}' for i in words]
+
+
+        
+
 labs = [f'bol cmd bias {i}' for i in range(4)] + labels0 + labels1 + labels2
 
-print(len(labs))
 
 #fig, axes = plt.subplots(sharex=True, sharey='row', nrows=6, ncols=4)
 fig, axes = plt.subplots(sharex=True, nrows=7, ncols=5)
@@ -131,5 +122,41 @@ axs = axes.flatten()
 for i in range(len(cmds[0])):
     axs[i].set_title(labs[i])
     axs[i].plot(cmds[:,i], '.', ms=1)
+
+
+cmds = bol_cmd_bias
+
+labels3 = ['bol_volt', 'cna_temp', 'dbx_tmp', 'hot_spot', 'ipdu_amp',
+        'ipdu_volt', 'ipdu_temp', 'mtm_cal_mtr', 'mtm_pos', 'pamp_chan',
+        'pamp_op', 'stat_mon_temp', 'temp_ctrl']
+
+for lab in labels3:
+    word = data[f'en_analog/group1/{lab}'][()]
+    if len(word.shape) == 0:
+        cmds = np.hstack((cmds, word[:,np.newaxis]))
+    else:
+        cmds = np.hstack((cmds, word))
+
+labels3 = [f'bol_volt_{i}' for i in range(1,5)] + \
+          [f'cna_temp_{i}' for i in range(1,5)] + \
+          [f'dbx_tmp_{i}' for i in range(1,3)] + \
+          [f'hot_spot_{i}' for i in range(1, 3)] +  \
+          [f'ipdu_amp_{i:02}' for i in range(1, 13)] + \
+          [f'ipdu_volt_{i:02}' for i in range(1, 21)] + \
+          [f'ipdu_temp_{i:02}' for i in range(1, 3)] +  \
+          [f'mtm_cal_mtr_{i:02}' for i in range(1,3)] + \
+          [f'mtm_pos_{i:02}' for i in range(1,3)] +  \
+          ['pamp_chan', 'pamp_op', 'stat_mon_temp_1', 'stat_mon_temp_2'] +  \
+          [f'temp_ctrl_{i}' for i in range(1,9)]
+
+labs = labels3
+
+fig, axes = plt.subplots(sharex=True, nrows=8, ncols=8)
+
+axs = axes.flatten()
+
+for i in range(len(labs)):
+    axs[i].set_title(labs[i])
+    axs[i].plot(cmds[:,i+4], '.', ms=1, alpha=0.5)
 
 plt.show()
