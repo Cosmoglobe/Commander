6bece0bc7efecadfaf51d5599cd2d97e81498e77
Hans Kristian Eriksen <h.k.k.eriksen@astro.uio.no>
2025-01-17 13:11:13 +0100

    Fixed alpha vs albedo bug

--- commander3/src/comm_zodi_mod.f90
+++ commander3/src/comm_zodi_mod.f90
@@ -127,7 +127,7 @@ contains
          call get_tokens(cpar%zs_samp_group_bands(i), ',', tokens, ntok) 
          do j = 1, ntok
             k = get_string_index(band_labels, tokens(j))
-            zodi_model%sampgroup_active_band(k,i) = .true.
+            zodi_model%sampgroup_active_band(k,i) = .true. 
          end do
          call samp_group2stat(cpar, i, zodi_model%sampgroup_active_band(:,i), zodi_model%theta_stat(:,i))
       end do
@@ -1024,7 +1024,7 @@ contains
                  ind = zodi_model%get_par_ind(comp=zodi_model%comps(c), em_string=comp_param(2))
                  stat(ind) = 0
               end if
-           else if (trim(label(1:2)) == 'al') then
+           else if (trim(label(1:2)) == 'al' .and. trim(label) /= 'alpha') then
               ! Albedo
               call get_tokens(label, '@', comp_param, num=n)
               if (n == 1) then
