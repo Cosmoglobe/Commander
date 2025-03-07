fixed median func, changed save paths to be global, fixed some stuff in mapmaker but there's still something wrong with the units

commander3/todscripts/firas/src/mapmaker/invert.py
@@ -39,11 +39,11 @@ def solve_m(P, N, d):
     return m
 
 if __name__ == "__main__":
-    frequencies = np.linspace(0, 13.604162 * SPEC_SIZE, SPEC_SIZE) # GHz - for SS
+    frequencies = np.linspace(0, 13.604162/2 * SPEC_SIZE, SPEC_SIZE)[1:] # GHz - for SS
     frequencies_icm = (frequencies * u.GHz).to(1 / u.cm, equivalencies=u.spectral()).value # cm-1
     x = np.linspace(
         0, 1.76, IFG_SIZE
-    ) # cm
+    )[1:] # cm
 
     d = np.load("tests/ifgs.npz")['ifg'][:, 1:]
     # find where nans are in d
@@ -52,20 +52,17 @@ if __name__ == "__main__":
     P = np.zeros((d.shape[1], d.shape[1]))
     for i in range(d.shape[1]):
         for j in range(d.shape[1]):
-            P[i, j] = np.abs(frequencies[i] - frequencies[j]) * np.sum(np.cos(2 * np.pi * frequencies_icm[i] * (x - 1.22))) # for SS - TODO: double check if this is correct
-
-    # check for nans
-    print(f"nans in P: {np.isnan(P).any()}")
-    print(f"nans in N: {np.isnan(N).any()}")
-    print(f"nans in d: {np.isnan(d).any()}")
+            P[i, j] = np.abs(frequencies[i] - frequencies[j]) * np.sum(np.cos(2 * np.pi * frequencies_icm[i] * (x - 1.22))) # for SS
 
     m = np.zeros((d.shape[0], d.shape[1]))
     for pixel in range(d.shape[0]):
         print(f"solving for pixel {pixel}")
         m[pixel] = solve_m(P, N, d[pixel])
-        # print(f"m at pixel {pixel}: {m[pixel]}")
+        
+    # save map output
+    np.savez("tests/m_invert.npz", m=m)
 
     # plot the result for a frequency
-    hp.mollview(m[:, 63], title="m at frequency 857 GHz") # TODO: units?
-    plt.savefig("tests/m.png")
+    # hp.mollview(m[:, 63], title="m at frequency 857 GHz", min=0, max=200) # TODO: units?
+    # plt.savefig("tests/m.png")
         

commander3/todscripts/firas/src/mapmaker/plot.py
@@ -1,14 +1,36 @@
+import os
+
 import healpy as hp
 import matplotlib.pyplot as plt
+import numpy as np
 from sim import sim_dust
 
-dust_map_downgraded_mjy, frequencies, signal = sim_dust()
 
-# plot map for each frequency
-for i in range(len(frequencies)):
-    print(f"Plotting dust map for frequency {i}")
-    dust_map = dust_map_downgraded_mjy * signal[i]
-    hp.mollview(dust_map, title=f"{int(frequencies[i]):04d} GHz", unit="MJy/sr", min=0, max=200)
-    plt.savefig(f"tests/dust_maps/{int(frequencies[i]):04d}.png")
-    plt.close()
-    plt.clf()
+def plot_dust_maps():
+    # plot map for each frequency
+    for i in range(len(frequencies)):
+        print(f"Plotting dust map for frequency {i}")
+        dust_map = dust_map_downgraded_mjy * signal[i]
+        hp.mollview(dust_map, title=f"{int(frequencies[i]):04d} GHz", unit="MJy/sr", min=0, max=200)
+        plt.savefig(f"tests/dust_maps/{int(frequencies[i]):04d}.png")
+        plt.close()
+        plt.clf()
+
+def plot_m_invert(frequencies):
+    # clean previous maps
+    for file in os.listdir("tests/m_invert"):
+        os.remove(f"tests/m_invert/{file}")
+
+    m = np.load("tests/m_invert.npz")['m']
+    frequencies = frequencies[1:]
+    for i in range(m.shape[1]):
+        # print(f"Plotting m for frequency {i}")
+        hp.mollview(m[:, i], title=f"{int(frequencies[i]):04d} GHz", min=0, max=200)
+        plt.savefig(f"tests/m_invert/{int(frequencies[i]):04d}.png")
+        plt.close()
+        plt.clf()
+
+if __name__ == "__main__":
+    dust_map_downgraded_mjy, frequencies, signal = sim_dust()
+    # plot_dust_maps()
+    plot_m_invert(frequencies)

commander3/todscripts/firas/src/mapmaker/sim.py
@@ -41,7 +41,7 @@ def sim_dust():
     #     plt.clf()
     #     plt.close()
 
-    frequencies = np.linspace(0, 13.604162 * SPEC_SIZE, SPEC_SIZE)
+    frequencies = np.linspace(0, 13.604162/2 * SPEC_SIZE, SPEC_SIZE)
 
     signal = dust(frequencies, nu0_dust, beta_dust, t_dust)
 

commander3/todscripts/firas/src/my_utils.py
@@ -7,7 +7,6 @@ from utils.frd import elex_transfcnl
 from utils.fut import get_recnum
 
 
-# @njit(parallel=True)
 def calculate_dc_response(bol_cmd_bias, bol_volt, Jo, Jg, Tbol, rho, R0, T0, beta, G1):
     """
     Taken largely from calc_responsivity in fsl.
@@ -17,56 +16,32 @@ def calculate_dc_response(bol_cmd_bias, bol_volt, Jo, Jg, Tbol, rho, R0, T0, bet
 
     cmd_bias = bol_cmd_bias.astype(
         "double"
-    )  # / 25.5  # only use the factor for when it's not in volts? i.e. don't use for the data from lambda
-
-    # print("cmd_bias:", cmd_bias)
+    )
 
     V = (bol_volt - Jo) / Jg
-
-    # print("V:", V)
 
     RL = 4.0e7
     R = RL * V / (cmd_bias - V)
 
-    # print("R:", R, "RL:", RL)
-
     X = V * rho
     Y = R / R0 / X
-
-    # print("X:", X, "Y:", Y)
 
     SQ = np.log(Y * Tbol * np.sinh(X / Tbol))
     Tbol = T0 / SQ**2
 
-    # print("SQ:", SQ, "Tbol:", Tbol)
-
     SQ = np.log(Y * Tbol * np.sinh(X / Tbol))
-
-    # print("SQ:", SQ)
 
     Tbol = T0 / (SQ * SQ)
 
-    # print("Tbol:", Tbol)
-
     G = G1 * Tbol**beta
-
-    # print("G:", G)
 
     H = Tbol / X * np.tanh(X / Tbol)
 
-    # print("H:", H)
-
     DT = 1.0 / H - 1.0 - 0.5 * np.sqrt(T0 / Tbol)
-
-    # print("DT:", DT)
 
     Z = (G * Tbol * R + DT * V**2) / (G * Tbol * R / H - DT * V**2)
 
-    # print("Z:", Z)
-
     S0 = rscale * R * (Z - H) / (V * (Z * R / RL + 1.0) * (H + 1.0))
-
-    # print("S0:", S0)
 
     return S0
 
@@ -98,7 +73,6 @@ def calculate_time_constant(
 
     return tau
 
-
 # @njit(parallel=True)
 def clean_ifg(
     ifg,
@@ -112,7 +86,7 @@ def clean_ifg(
 ):
     # print("ifg shape before subtraction:", ifg.shape)
 
-    median_ifg = np.expand_dims(np.median(ifg), axis=-1)
+    median_ifg = np.expand_dims(np.median(ifg, axis=1), axis=-1)
 
     # subtract dither
     ifg = ifg - median_ifg

commander3/todscripts/firas/src/temperature_map/firas_curve.py
@@ -1,18 +1,22 @@
+import os
+import sys
+
 import h5py
 import healpy as hp
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
+from globals import PROCESSED_DATA_PATH
 from scipy.optimize import minimize
 from utils.config import gen_nyquistl
-import os
-import sys
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 from my_utils import planck, residuals
 
+user = os.environ["USER"]
+save_path = f"/mn/stornext/d16/www_cmb/{user}/firas/"
 
 T_CMB = 2.72548  # Fixsen 2009
 modes = {"ss": 0, "lf": 3}
@@ -21,8 +25,7 @@ channels = {"rl": 1, "ll": 3}
 mask = fits.open("BP_CMB_I_analysis_mask_n1024_v2.fits")
 mask = mask[1].data.astype(int)
 
-data = np.load("../../output/data/sky.npz")
-# print(data.files)
+data = np.load(PROCESSED_DATA_PATH)
 
 sky = {}
 pix_gal = {}
@@ -30,63 +33,6 @@ for channel in channels.keys():
     for mode in modes.keys():
         sky[f"{channel}_{mode}"] = np.abs(data[f"{channel}_{mode}"])
         pix_gal[mode] = data[f"pix_gal_{mode}"]
-
-# stat_word_1 = np.array(data["df_data/stat_word_1"][()]).astype(int)
-# stat_word_12 = np.array(data["df_data/stat_word_12"][()]).astype(int)
-# stat_word_9 = np.array(data["df_data/stat_word_9"][()]).astype(int)
-# lvdt_stat_b = np.array(data["df_data/lvdt_stat_b"][()]).astype(int)
-# stat_word_13 = np.array(data["df_data/stat_word_13"][()]).astype(int)
-# stat_word_16 = np.array(data["df_data/stat_word_16"][()]).astype(int)
-
-
-# to not use ICAL higher than 3 temps
-# a_ical = np.array(data["df_data/a_ical"][()])
-# b_ical = np.array(data["df_data/b_ical"][()])
-# ical = (a_ical + b_ical) / 2
-# ical_lower_3 = ical < 3
-# pix_gal = pix_gal[ical_lower_3]
-# sky = sky[ical_lower_3]
-
-# remove data that i flagged
-# stat_word_1 = stat_word_1[short_filter & slow_filter]
-# stat_word_12 = stat_word_12[short_filter & slow_filter]
-# stat_word_9 = stat_word_9[short_filter & slow_filter]
-# lvdt_stat_b = lvdt_stat_b[short_filter & slow_filter]
-# stat_word_13 = stat_word_13[short_filter & slow_filter]
-# stat_word_16 = stat_word_16[short_filter & slow_filter]
-
-# filter1 = stat_word_1 != 46
-# filter2 = stat_word_12 != 19121
-# filter3 = stat_word_9 != 45110
-# filter4 = stat_word_12 != 18536
-# filter5 = stat_word_12 != 54906
-# filter6 = lvdt_stat_b != 83
-# filter7 = stat_word_12 != 63675
-# filter8 = stat_word_13 != 19585
-# filter9 = stat_word_16 != 14372
-
-# pix_gal = pix_gal[
-#     filter1
-#     & filter2
-#     & filter3
-#     & filter4
-#     & filter5
-#     & filter6
-#     & filter7
-#     & filter8
-#     & filter9
-# ]
-# sky = sky[
-#     filter1
-#     & filter2
-#     & filter3
-#     & filter4
-#     & filter5
-#     & filter6
-#     & filter7
-#     & filter8
-#     & filter9
-# ]
 
 # frequency mapping
 nu0 = {"ss": 68.020812, "lf": 23.807283}
@@ -171,5 +117,5 @@ for channel in channels.keys():
         plt.ylabel("Brightness [MJy/sr]")
         plt.title("BB curve")
         plt.legend()
-        plt.savefig(f"../../output/plots/bb_curve_{f"{channel}_{mode}"}.png")
+        plt.savefig(f"{save_path}/plots/bb_curve/{f"{channel}_{mode}"}.png")
         plt.clf()

commander3/todscripts/firas/src/temperature_map/main.py
@@ -2,23 +2,21 @@
 This script takes the interferograms into spectra.
 """
 
+import os
+import sys
 from datetime import datetime
 
 import h5py
 import healpy as hp
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from astropy.io import fits
+from globals import PROCESSED_DATA_PATH
 from utils.config import gen_nyquistl
-import os
-import sys
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 from my_utils import clean_ifg, filter_junk, ifg_to_spec, planck, tune_pointing
-
 
 T_CMB = 2.72548  # Fixsen 2009
 NSIDE = 32
@@ -406,8 +404,7 @@ for channel, channel_value in channels.items():
                 channel=channel_value,
                 adds_per_group=variablesm[f"adds_per_group_{mode}"],
                 # adds_per_group=variablesm[f"adds_per_group_{channel}_{mode}"],
-                bol_cmd_bias=variablesm[f"bol_cmd_bias_{channel}_{mode}"]
-                / 25.5,  # needs this factor to put it into volts (from pipeline)
+                bol_cmd_bias=variablesm[f"bol_cmd_bias_{channel}_{mode}"] / 25.5,  # needs this factor to put it into volts (from pipeline)
                 bol_volt=variablesm[f"bol_volt_{channel}_{mode}"],
                 fnyq_icm=fnyq["icm"][frec[f"{channel}_{mode}"]],
                 fnyq_hz=fnyq["hz"][frec[f"{channel}_{mode}"]],
@@ -660,12 +657,8 @@ for variable in extra_variables:
         ]
 
 # save the sky
-if OFFSET == 0:
-    save_name = "../../data/processed_sky.npz"
-else:
-    save_name = f"../../data/processed_sky_offset_{OFFSET}.npz"
 np.savez(
-    save_name,
+    PROCESSED_DATA_PATH,
     **variablesm,
     **sky,
 )

commander3/todscripts/firas/src/temperature_map/plot_map.py
@@ -3,15 +3,13 @@ Script to take the previously generated sky spectra (sky.npy) and plot a map wit
 """
 
 import os
+import sys
 from pathlib import Path
 
 import healpy as hp
 import matplotlib.pyplot as plt
 import numpy as np
-from globals import COORDINATES, OFFSET, T_CMB
-
-import os
-import sys
+from globals import COORDINATES, OFFSET, PROCESSED_DATA_PATH, T_CMB
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -24,7 +22,7 @@ save_path = f"/mn/stornext/d16/www_cmb/{user}/firas/"
 modes = {"ss": 0, "lf": 3}
 channels = {"rh": 0, "rl": 1, "lh": 2, "ll": 3}
 
-data = np.load(f"../../data/processed_sky_offset_{OFFSET}.npz")
+data = np.load(PROCESSED_DATA_PATH)
 # print(data.files)
 
 sky = {}