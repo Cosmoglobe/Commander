46308eb12caaab1f95e842c8379dcdc672c5b2ad
Duncan <duncanjosephwatts@gmail.com>
2025-07-14 11:20:34 +0200

    Updated install_ita for new owls

--- install_ita.sh
+++ install_ita.sh
@@ -21,7 +21,7 @@ owl2930="$prefix+(29|30)+$suffix"
 owl3135="$prefix+(3[1-5])+$suffix"
 owl3637="$prefix+(3[6-7])+$suffix"
 owl38="$prefix+(38)+$suffix"
-owl4046="$prefix+(4[0-6])+$suffix"
+owl3946="$prefix+(39|4[0-6])+$suffix"
 # Using regex to figure out which beehive I am on.
 prefix="beehive"
 bee0123="$prefix+(\d{0}|[1-9](?!\d)|1[0-9]|2[0-3])+$suffix"
@@ -96,8 +96,8 @@ then
     build_dir="build_owl3135_${toolchain}_${buildtype}"
   elif [[ "${HOSTNAME}" =~ $owl3637 ]]; then
     build_dir="build_owl3637_${toolchain}_${buildtype}"
-  elif [[ "${HOSTNAME}" =~ $owl4046 ]]; then
-    build_dir="build_owl4046_${toolchain}_${buildtype}"
+  elif [[ "${HOSTNAME}" =~ $owl3946 ]]; then
+    build_dir="build_owl3946_${toolchain}_${buildtype}"
   elif [[ "${HOSTNAME}" =~ $owl38 ]]; then
     build_dir="build_owl38_${toolchain}_${buildtype}"
   elif [[ "${HOSTNAME}" =~ $bee0123 ]]; then
