4bba6961e5a978f8e1b9eb4af75653fbb4961936
Ana Martins <a.i.s.martins@astro.uio.no>
2025-07-31 13:56:36 +0200

    remove unused variables from main_cal

--- commander3/todscripts/firas/src/simulations/short_otf.py
+++ commander3/todscripts/firas/src/simulations/short_otf.py
@@ -12,7 +12,6 @@ import globals as g
 sky_data = np.load(g.PROCESSED_DATA_PATH_CAL)
 print(sky_data.files)
 
-ifgs_ll = sky_data["df_data/ifg_ll"]
-
+ifgs_ll = sky_data["ifg_ll_ss"]
 
 temp_xcal = sky_data["df_data"]
