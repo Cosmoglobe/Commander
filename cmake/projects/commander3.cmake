3f78d673e9631cfbcc200db1ba4cb7c48ac4bb5f
Duncan <duncanjosephwatts@gmail.com>
2025-08-26 10:53:48 +0200

    Added cmake config files

--- cmake/projects/commander3.cmake
+++ cmake/projects/commander3.cmake
@@ -60,6 +60,7 @@ set(sources
 	${COMMANDER3_SOURCE_DIR}/comm_tod_noise_psd_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_pixhist_mod.f90
+	${COMMANDER3_SOURCE_DIR}/comm_tod_pixcache_smod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_driver_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_mapmaking_mod.f90
 	${COMMANDER3_SOURCE_DIR}/comm_tod_lfi_mod.f90
