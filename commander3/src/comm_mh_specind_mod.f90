95c4e205f20b357df5da1971b2aabdbf3d06c46a
Raelyn Sullivan <raelyn.m.sullivan@gmail.com>
2026-05-12 10:22:21 +0200

    merge conflict

--- commander3/src/comm_mh_specind_mod.f90
+++ commander3/src/comm_mh_specind_mod.f90
@@ -432,7 +432,11 @@ contains
                  write(*,fmt='(a,a12)',advance='no') ' |   ', trim(c%label)
                  do i = 1, c%ntab
                     if (c%theta_steplen(c%npar+i,l) > 0) then
-                       write(*,fmt='(a,i4,a,f16.8)',advance='no') ', bin = ', i, ', old = ', c%SEDtab(c%npar_tab+2,i)
+                       if (i == 1) then
+                        write(*,fmt='(a,i4,a,f16.8)',advance='no') ', bin = ', i, ', old = ', c%SEDtab(c%npar_tab+2,i)
+                       else
+                        write(*,fmt='(a,i4,a,f16.8)',advance='no') ' |                 bin = ', i, ', old = ', c%SEDtab(c%npar_tab+2,i)
+                       end if
                        do j = c%npar_tab+2, size(c%SEDtab(:,i))
                           c%SEDtab(j,i) = c%SEDtab(j,i) + rand_gauss(handle) * c%theta_steplen(c%npar+i,l) * mh_scale(l)
                        end do
