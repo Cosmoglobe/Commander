92d41678d6dc7559d05ab1a1ff665be4a1d51c01
Duncan <duncanjosephwatts@gmail.com>
2026-06-05 00:43:09 +0200

    Added extra check for optional variable

--- commander3/src/comm_chisq_mod.f90
+++ commander3/src/comm_chisq_mod.f90
@@ -582,16 +582,17 @@ contains
     end if
 
     ! Clean up
-    nullify(c)
     if (associated(gaindiff)) then
       call gaindiff%dealloc()
       deallocate(gaindiff)
       nullify(gaindiff)
     end if
-    if (associated(gainmap)) then
-      call gainmap%dealloc()
-      deallocate(gainmap)
-      nullify(gainmap)
+    if (present(gainmap)) then
+      if (associated(gainmap)) then
+        call gainmap%dealloc()
+        deallocate(gainmap)
+        nullify(gainmap)
+      end if
     end if
     call map_diff%dealloc; deallocate(map_diff)
     if (present(cmbmap)) then
