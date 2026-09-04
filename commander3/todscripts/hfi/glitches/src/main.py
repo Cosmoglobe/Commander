20c5954c4a99cee266a72460219a838fea5066cb
Ana Martins <a.i.s.martins@astro.uio.no>
2026-09-04 12:05:44 +0200

    added new template estimation and starting on 1st iteration

--- commander3/todscripts/hfi/glitches/src/main.py
+++ commander3/todscripts/hfi/glitches/src/main.py
@@ -13,29 +13,14 @@ def main():
     res = np.load(f"{g.DATA_PATH}143-2a_simulations.npy")
     seconds = np.linspace(0, len(res) / g.SAMPRATE, len(res))
 
+    print("Iteration 0")
+
     print("Starting glitch detection...")
     glitch_idx = detection.matched_filter(res, seconds)
     print(f"Detected {len(glitch_idx)} glitches at indices: {glitch_idx}")
 
     print("Starting glitch classification...")
     glitch_labels, glitch_amps = classification.classify_glitches(glitch_idx, res, seconds)
-
-    # if g.PLOTS:
-        # T = subtraction.build_template_matrix(glitch_idx, seconds, glitch_labels, glitch_amps)
-
-        # im = plt.imshow(T, aspect='auto', norm='log')
-        # plt.colorbar(im, label='Amplitude')
-        # plt.savefig(f"{g.FIGURES_PATH}debug/template_matrix.png")
-        # plt.close()
-
-        # b = subtraction.calculate_b(res, T)
-        # A = subtraction.calculate_A(T)
-
-        # plt.imshow(A, aspect='auto', norm='log')
-        # plt.colorbar(label='Amplitude')
-        # plt.title("Matrix A = T^T N^-1 T")
-        # plt.savefig(f"{g.FIGURES_PATH}debug/matrix_A.png")
-        # plt.close()
 
     print("Starting glitch subtraction...")
     result, fitted_amps = subtraction.subtract_glitches_from_data(
@@ -53,11 +38,32 @@ def main():
         plt.savefig(f"{g.FIGURES_PATH}debug/glitch_subtraction_result_0.png")
         plt.close()
 
-    templates.stacking(result, glitch_idx, glitch_labels, fitted_amps, seconds)
+    final_stack = templates.stacking(result, glitch_idx, glitch_labels, fitted_amps, seconds)
 
-    # maxiter = 1
-        # for i in range(maxiter):
+    for glitch_type in ['short', 'long', 'slow']:
+        amps, taus = templates.glitch_estimation(seconds[:int(g.NSECS * g.SAMPRATE)],
+                                           final_stack[glitch_type])
+        # print(f"Estimated parameters for {glitch_type} glitch: amplitudes={amps}, taus={taus}")
+        if g.PLOTS:
+            plt.plot(seconds[:int(2*g.SAMPRATE)], final_stack[glitch_type][:int(2*g.SAMPRATE)],
+                    label="Stacked median")
+            plt.plot(seconds[:int(2*g.SAMPRATE)],
+                    templates.glitch_model(seconds[:int(g.NSECS * g.SAMPRATE)], *amps, *taus)[:int(2*g.SAMPRATE)],
+                    label="Fitted Model")
+
+            plt.title(f"Glitch Stacking and Fitting for {glitch_type} Glitches")
+            plt.xlabel("Time (s)")
+            plt.ylabel("Amplitude")
+            plt.legend()
+            plt.savefig(f"{g.FIGURES_PATH}templates/glitch_fitting_{glitch_type}.png")
+            plt.close()
+
+    maxiter = 1
+    for i in range(maxiter):
+        print(f"Iteration {i+1}/{maxiter}")
+        print("Starting glitch detection...")
+        glitch_idx = detection.matched_filter(res, seconds)
 
 if __name__ == "__main__":
-    cProfile.run('main()', sort='time')
-    # main()
+    # cProfile.run('main()', sort='time')
+    main()
